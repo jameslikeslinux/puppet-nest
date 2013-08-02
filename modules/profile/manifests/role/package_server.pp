82b1db1aed902f59ee178c87d07a7d53e4e46b3d
James Lee <jlee@thestaticvoid.com>
2013-08-02 17:55:30 -0400

    Set package server to http://packages.thestaticvoid.com

--- modules/profile/manifests/role/package_server.pp
+++ modules/profile/manifests/role/package_server.pp
@@ -2,7 +2,7 @@ class profile::role::package_server {
     include role::web_server
 
     class { 'package::server':
-        short_name  => 'pkg',
+        short_name  => 'packages',
         domain_name => 'thestaticvoid.com', # or $domain
     }
 }
--- nodes/test.pp
+++ nodes/test.pp
@@ -3,7 +3,7 @@ node 'test.thestaticvoid.com' {
         disk_profile       => cryptmirror,
         console_resolution => '1024x768',
         video_cards        => ['cirrus'],
-        package_server     => 'http://hawk.thestaticvoid.com/packages',
+        package_server     => 'http://packages.thestaticvoid.com/',
         roles              => ['desktop'],
     }
 }
