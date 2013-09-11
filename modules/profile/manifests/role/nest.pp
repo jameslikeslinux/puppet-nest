d67f95074309479d93c743818b007f5d28aa7c48
James Lee <jlee@thestaticvoid.com>
2013-09-11 12:17:12 -0400

    nest: Add MusicBrainz Picard

--- modules/profile/manifests/role/nest.pp
+++ modules/profile/manifests/role/nest.pp
@@ -1,3 +1,13 @@
 class profile::role::nest {
     class { 'nfs::server': }
+
+    package_use { 'media-libs/chromaprint':
+        use     => 'tools',
+        version => '>=0.7',
+    }
+
+    portage::package { 'media-sound/picard':
+        ensure  => installed,
+        require => Package_use['media-libs/chromaprint'],
+    }
 }
