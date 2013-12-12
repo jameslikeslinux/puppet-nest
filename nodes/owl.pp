103c77e22be7325962c1cc6969d89e3c9662dd02
James Lee <jlee@thestaticvoid.com>
2013-12-12 17:26:41 -0500

    owl: Update disk id

--- nodes/owl.pp
+++ nodes/owl.pp
@@ -1,6 +1,6 @@
 node 'owl' {
     class { 'profile::base':
-        disk_path      => 'pci-0000:00:1f.2-scsi-0:0:0:0',
+        disk_id        => 'ata-SAMSUNG_HM060HI_S10XJ30P523514',
         disk_profile   => crypt,
         video_cards    => ['nouveau'],
         package_server => 'http://packages.thestaticvoid.com/',
