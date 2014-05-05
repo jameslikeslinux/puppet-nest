7876f45f72a7d44bc2dc566f0769698e4ba3b38f
James Lee <jlee@thestaticvoid.com>
2014-05-05 00:43:40 -0400

    users: Separate out sshkey defined type

--- modules/users/manifests/user.pp
+++ modules/users/manifests/user.pp
@@ -36,27 +36,10 @@ define users::user (
     }
 
     if $ssh_key_source {
-        $filename = regsubst($ssh_key_source, '^.*/([^/]+)$', '\1')
-
-        file { "${home}/.ssh":
-            ensure => directory,
-            mode   => '0644',
-            owner  => $name,
-            group  => $gid,
-        }
-
-        file { "${home}/.ssh/${filename}":
-            mode    => '0600',
-            owner   => $name,
-            group   => $gid,
-            source  => $ssh_key_source,
-        }
-
-        file { "${home}/.ssh/${filename}.pub":
-            mode    => '0644',
-            owner   => $name,
-            group   => $gid,
-            source  => "${ssh_key_source}.pub",
+        users::sshkey { $name:
+            ssh_key_source => $ssh_key_source,
+            home           => $home,
+            gid            => $gid,
         }
     }
 }
