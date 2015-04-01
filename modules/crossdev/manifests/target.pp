de1dca549f0b782db4070e8bd3f2fef814fb5af4
James Lee <jlee@thestaticvoid.com>
2015-03-31 23:19:51 -0400

    crossdev: Funtoo supports crossdev again!

--- modules/crossdev/manifests/target.pp
+++ modules/crossdev/manifests/target.pp
@@ -3,10 +3,7 @@ define crossdev::target (
 ) {
     include crossdev
 
-    # Funtoo has very limited support for Crossdev
-    # See: https://bugs.funtoo.org/browse/FL-652
-    #      https://bugs.funtoo.org/browse/FL-821
-    exec { "/usr/bin/crossdev --stable --gcc 4.8.3-r1 --target ${target}":
+    exec { "/usr/bin/crossdev --stable --target ${target}":
         timeout => 0,
         creates => "/usr/bin/${target}-gcc",
         require => Class['crossdev'],
