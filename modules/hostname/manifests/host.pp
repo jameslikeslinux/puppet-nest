6474616faaa133d925d078dd7f6030581df5b5de
James Lee <jlee@thestaticvoid.com>
2015-04-08 12:10:54 -0400

    hosts: I wish Funtoo would make up their mind about this

--- modules/hostname/manifests/host.pp
+++ modules/hostname/manifests/host.pp
@@ -1,25 +1,25 @@
 define hostname::host (
     $ip,
 ) {
-    if $osfamily == 'gentoo' {
-        include hostname
-
-        # Pad IP with zeros to make it sortable
-        # XXX: This only works with IPv4!  Also, there is no error checking.
-        $sortable_ip = inline_template('<%= "%03d.%03d.%03d.%03d" % @ip.split(".") %>')
-
-        concat::fragment { "hostname-alias-${sortable_ip}-${name}":
-            content => "aliases+=\"${ip}\t${name}\n\"\n",
-            target  => '/etc/conf.d/hostname',
-        }
-
-        host { $name:
-            ip      => $ip,
-            require => Class['hostname'],
-        }
-    } else {
+#    if $osfamily == 'gentoo' {
+#        include hostname
+#
+#        # Pad IP with zeros to make it sortable
+#        # XXX: This only works with IPv4!  Also, there is no error checking.
+#        $sortable_ip = inline_template('<%= "%03d.%03d.%03d.%03d" % @ip.split(".") %>')
+#
+#        concat::fragment { "hostname-alias-${sortable_ip}-${name}":
+#            content => "aliases+=\"${ip}\t${name}\n\"\n",
+#            target  => '/etc/conf.d/hostname',
+#        }
+#
+#        host { $name:
+#            ip      => $ip,
+#            require => Class['hostname'],
+#        }
+#    } else {
         host { $name:
             ip => $ip,
         }
-    }
+#    }
 }
