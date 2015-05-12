3f372260a06e9326b22f9d5f6e706732f2821e97
James Lee <jlee@thestaticvoid.com>
2015-05-12 11:08:20 -0400

    crossdev: Set mask for gcc

--- modules/crossdev/manifests/target.pp
+++ modules/crossdev/manifests/target.pp
@@ -3,7 +3,13 @@ define crossdev::target (
 ) {
     include crossdev
 
-    exec { "/usr/bin/crossdev --stable --target ${target}":
+    package_mask { "cross-${target}/gcc":
+        version => '>4.8.4',
+        before  => Exec["crossdev-${target}"],
+    }
+
+    exec { "crossdev-${target}":
+        command => "/usr/bin/crossdev --stable --target ${target}",
         timeout => 0,
         creates => "/usr/bin/${target}-gcc",
         require => Class['crossdev'],
