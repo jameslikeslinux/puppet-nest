6dd740684b88e31fb93a9495db8684c29ee2e1b3
James Lee <jlee@thestaticvoid.com>
2016-04-03 22:59:17 -0400

    Add root home directory

--- Puppetfile
+++ Puppetfile
@@ -1,4 +1,5 @@
 mod 'gentoo/portage', '2.3.0'
 mod 'puppetlabs/concat', '2.1.0'
 mod 'puppetlabs/stdlib', '4.11.0'
+mod 'puppetlabs/vcsrepo', '1.3.2'
 mod 'theforeman/puppet', '4.3.0'
--- tmp/nest/manifests/profile/setup/root.pp
+++ tmp/nest/manifests/profile/setup/root.pp
@@ -7,4 +7,15 @@ class nest::profile::setup::root {
     shell   => '/bin/zsh',
     require => Package['app-shells/zsh'],
   }
+
+  file { '/root/.keep':
+    ensure => absent,
+  }
+
+  vcsrepo { '/root':
+    ensure   => latest,
+    provider => git,
+    source   => 'https://github.com/MrStaticVoid/profile.git',
+    require  => File['/root/.keep'],
+  }
 }
