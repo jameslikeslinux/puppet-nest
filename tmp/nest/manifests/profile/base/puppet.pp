25738c368fc9e0789c5c0d80ef37785094c472c1
James Lee <jlee@thestaticvoid.com>
2016-04-09 22:15:54 -0400

    puppet: Add server config

--- tmp/nest/manifests/profile/base/puppet.pp
+++ tmp/nest/manifests/profile/base/puppet.pp
@@ -1,7 +1,49 @@
 class nest::profile::base::puppet {
   if $::nest::server {
     class { '::puppet':
-      unavailable_runmodes => ['cron'],
+      dns_alt_names         => ['nest.james.tl'],
+      server                => true,
+      server_environments   => [],
+      server_foreman        => false,
+      server_implementation => 'puppetserver',
+      server_jvm_config     => '/etc/conf.d/puppetserver',
+      unavailable_runmodes  => ['cron'],
+    }
+
+    package { 'hiera-eyaml':
+      ensure   => installed,
+      provider => puppetserver_gem,
+    }
+
+    file { '/etc/puppetlabs/code/hiera.yaml':
+      mode => '0644',
+      owner => 'root',
+      group => 'root',
+      source => 'puppet:///modules/nest/puppet/hiera.yaml'
+    }
+
+    Class['::puppet::server::install'] ->
+    Package['hiera-eyaml'] ->
+    File['/etc/puppetlabs/code/hiera.yaml'] ~>
+    Class['::puppet::server::service']
+
+    package { 'r10k':
+      ensure   => installed,
+      provider => puppet_gem,
+    }
+
+    file { '/etc/puppetlabs/r10k':
+      ensure => directory,
+      mode   => '0755',
+      owner  => 'root',
+      group  => 'root',
+    }
+
+    file { '/etc/puppetlabs/r10k/r10k.yaml':
+      mode   => '0644',
+      owner  => 'root',
+      group  => 'root',
+      source => 'puppet:///modules/nest/puppet/r10k.yaml',
     }
   } else {
     class { '::puppet':
@@ -9,7 +51,7 @@ class nest::profile::base::puppet {
     }
   }
 
-  package { 'hiera-eyaml':
+  package { 'dev-ruby/hiera-eyaml':
     ensure =>  installed,
   }
 
