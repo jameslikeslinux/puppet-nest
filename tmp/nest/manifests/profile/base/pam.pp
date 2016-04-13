9e3837ae00d0a1b95936f1c7c29659cc8da8fe07
James Lee <jlee@thestaticvoid.com>
2016-04-13 01:44:16 -0400

    pam: Use augeas to mangle system-auth

--- tmp/nest/manifests/profile/base/pam.pp
+++ tmp/nest/manifests/profile/base/pam.pp
@@ -3,18 +3,44 @@ class nest::profile::base::pam {
     use => ['pam_krb5', 'pam_ssh'],
   }
 
-  file { '/etc/krb5.conf':
-    mode   => '0644',
-    owner  => 'root',
-    group  => 'root',
-    source => 'puppet:///modules/nest/pam/krb5.conf',
-  }
+  $krb5_conf = @(EOT)
+    [libdefaults]
+        default_realm = UMD.EDU
+        forwardable = yes
+    | EOT
 
-  file { '/etc/pam.d/system-auth':
+  file { '/etc/krb5.conf':
     mode    => '0644',
     owner   => 'root',
     group   => 'root',
-    source  => 'puppet:///modules/nest/pam/system-auth',
-    require => Nest::Portage::Package_use['sys-auth/pambase'],
+    content => $krb5_conf,
   }
+
+  augeas { 'system-auth':
+    context => '/files/etc/pam.d/system-auth',
+    changes => [
+      # Do pam_krb5 before pam_ssh because pam_krb5 is optional
+      "ins 01 after *[type = 'auth' and module = 'pam_krb5.so']",
+      "mv *[type = 'auth' and module = 'pam_ssh.so'] 01",
+
+      # pam_krb5 is optional
+      "setm *[(type = 'auth' or type = 'session') and module = 'pam_krb5.so'] control optional",
+
+      # pam_krb5 should authenticate with the principal listed in ~/.k5login
+      "rm *[type = 'auth' and module = 'pam_krb5.so']/argument",
+      "set *[type = 'auth' and module = 'pam_krb5.so']/argument search_k5login",
+
+      # pam_ssh should use the password that pam_krb5 asked for
+      "rm *[type = 'auth' and module = 'pam_ssh.so']/argument",
+      "set *[type = 'auth' and module = 'pam_ssh.so']/argument use_first_pass",
+
+      # pam_krb5 should not have any effect on account status
+      # and should not be involved in password changes
+      "rm *[(type = 'account' or type = 'password') and module = 'pam_krb5.so']",
+
+      # pam_krb5 session doesn't need any arguments
+      "rm *[type = 'session' and module = 'pam_krb5.so']/argument",
+    ],
+    require => Nest::Portage::Package_use['sys-auth/pambase'],
+  } 
 }
