44a12a4a612fd55356c79782e2e2fde5057b2323
James Lee <jlee@thestaticvoid.com>
2016-04-14 21:26:10 -0400

    systemd: Set locale

--- tmp/nest/manifests/profile/base/systemd.pp
+++ tmp/nest/manifests/profile/base/systemd.pp
@@ -21,4 +21,20 @@ class nest::profile::base::systemd {
     changes => "set database[. = 'hosts']/service[last()+1] myhostname",
     onlyif  => "get database[. = 'hosts']/service[last()] != 'myhostname'",
   }
+
+  file_line { 'locale.gen-en_US.UTF-8':
+    path  => '/etc/locale.gen',
+    line  => 'en_US.UTF-8 UTF-8',
+    match => '^#en_US.UTF-8 UTF-8$',
+  }
+
+  exec { '/usr/sbin/locale-gen':
+    refreshonly => true,
+    subscribe   => File_line['locale.gen-en_US.UTF-8'],
+  }
+
+  file { '/etc/locale.conf':
+    content => "LANG=en_US.utf8\n",
+    require => Exec['/usr/sbin/locale-gen'],
+  }
 }
