6243c74b5e0804d5b9a90f17c7a7c744ef005919
James Lee <jlee@thestaticvoid.com>
2016-04-22 23:32:55 -0400

    Enable kwin triple buffering on nvidia

--- tmp/nest/manifests/profile/workstation/plasma.pp
+++ tmp/nest/manifests/profile/workstation/plasma.pp
@@ -11,6 +11,20 @@ class nest::profile::workstation::plasma {
     ensure => installed,
   }
 
+  $kwin_triple_buffer_ensure = $::nest::video_card ? {
+    'nvidia' => 'present',
+    default  => 'absent',
+  }
+
+  file { '/etc/plasma/startup/10-kwin-triple-buffer.sh':
+    ensure  => $kwin_triple_buffer_ensure,
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    content => "export KWIN_TRIPLE_BUFFER=1\n",
+    require => Package['kde-plasma/plasma-meta'],
+  }
+
   $gtk_scaling = @("EOT")
     export GDK_SCALE=${::nest::scaling_factor_rounded}
     export GDK_DPI_SCALE=${::nest::scaling_factor_percent_of_rounded}
