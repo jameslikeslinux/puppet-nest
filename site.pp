2338c48214c2666382ddeed1911ec47c9df66ed9
James Lee <jlee@thestaticvoid.com>
2016-04-23 01:26:41 -0400

    Refactor firewall rules

--- site.pp
+++ site.pp
@@ -2,21 +2,4 @@ Service {
   provider => systemd,
 }
 
-stage { 'pre': } -> Stage['main']
-
-class { 'nest':
-  stage => 'pre',
-}
-
-class { '::nest::firewall::pre':
-  stage => 'pre',
-}
-
-class { '::nest::firewall::post': }
-
-Firewall {
-  require => Class['::nest::firewall::pre'],
-  before  => Class['::nest::firewall::post'],
-}
-
 hiera_include('classes')
