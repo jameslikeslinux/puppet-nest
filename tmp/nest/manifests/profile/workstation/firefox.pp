969a08982bf0c32422d1661cafcf40261ad4db28
James Lee <jlee@thestaticvoid.com>
2016-04-23 23:16:42 -0400

    Add flash

--- tmp/nest/manifests/profile/workstation/firefox.pp
+++ tmp/nest/manifests/profile/workstation/firefox.pp
@@ -3,7 +3,11 @@ class nest::profile::workstation::firefox {
     use => 'gtk3',
   }
 
-  package { 'www-client/firefox':
+  package { [
+    'www-client/firefox',
+    'www-plugins/adobe-flash',
+    'media-libs/hal-flash',
+  ]:
     ensure => installed,
   }
 
@@ -13,5 +17,11 @@ class nest::profile::workstation::firefox {
     group   => 'root',
     content => "pref(\"layout.css.devPixelsPerPx\", \"${::nest::scaling_factor}\");\n",
     require => Package['www-client/firefox'],
+  }
+
+  exec { 'patch-flash-fullscreen-focus':
+    command => '/bin/sed -i "s/_NET_ACTIVE_WINDOW/_XET_ACTIVE_WINDOW/g" /usr/lib64/nsbrowser/plugins/libflashplayer.so',
+    onlyif  => '/bin/grep "_NET_ACTIVE_WINDOW" /usr/lib64/nsbrowser/plugins/libflashplayer.so',
+    require => Package['www-plugins/adobe-flash'],
   }
 }
