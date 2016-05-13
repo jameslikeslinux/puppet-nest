01e1c0599622c2a7f26ccfefe6a32ae0449e923b
James Lee <james@james.tl>
2016-05-12 22:48:29 -0400

    grub: Set font

--- tmp/nest/manifests/profile/base/grub.pp
+++ tmp/nest/manifests/profile/base/grub.pp
@@ -1,4 +1,6 @@
 class nest::profile::base::grub {
+  $font = "ter-x${::nest::console_font_size_nearest}b"
+
   nest::portage::package_use { 'sys-boot/grub':
     use => ['grub_platforms_efi-64', 'grub_platforms_pc', 'libzfs'],
   }
@@ -29,6 +31,11 @@ class nest::profile::base::grub {
     match => '^#?GRUB_FS=',
   }
 
+  file_line { 'grub-set-font':
+    line  => "GRUB_FONT=/boot/grub/fonts/${font}.pf2",
+    match => '^#?GRUB_FONT=',
+  }
+
   $::nest::grub_disks.each |$grub_disk| {
     $install_command = $grub_disk ? {
       /p(art)?\d$/ => "/bin/mkdir /boot/efi && /bin/mount ${grub_disk} /boot/efi && /usr/sbin/grub2-install --target=x86_64-efi --removable --modules=part_gpt && /bin/umount /boot/efi && /bin/rm -rf /boot/efi",
@@ -39,12 +46,18 @@ class nest::profile::base::grub {
       command     => $install_command,
       refreshonly => true,
       require     => Package['sys-boot/grub'],
-      before      => Exec['grub2-mkconfig'],
+      before      => Exec['grub2-mkfont'],
     }
+  }
+
+  exec { 'grub2-mkfont':
+    command => "/usr/bin/grub2-mkfont -o /boot/grub/fonts/${font}.pf2 /usr/share/fonts/terminus/${font}.pcf.gz",
+    creates => "/boot/grub/fonts/${font}.pf2"
   }
 
   exec { 'grub2-mkconfig':
     command     => '/usr/sbin/grub2-mkconfig -o /boot/grub/grub.cfg',
     refreshonly => true,
+    require     => Exec['grub2-mkfont'],
   }
 }
