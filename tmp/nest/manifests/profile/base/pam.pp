ff120d94a69ffdb421731925199c4d188d97e4af
James Lee <james@james.tl>
2016-05-12 23:07:34 -0400

    pam: Don't use pam_krb5

--- tmp/nest/manifests/profile/base/pam.pp
+++ tmp/nest/manifests/profile/base/pam.pp
@@ -1,46 +1,5 @@
 class nest::profile::base::pam {
   nest::portage::package_use { 'sys-auth/pambase':
-    use => ['pam_krb5', 'pam_ssh'],
+    use => 'pam_ssh',
   }
-
-  $krb5_conf = @(EOT)
-    [libdefaults]
-        default_realm = UMD.EDU
-        forwardable = yes
-    | EOT
-
-  file { '/etc/krb5.conf':
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
-    content => $krb5_conf,
-  }
-
-  augeas { 'system-auth':
-    context => '/files/etc/pam.d/system-auth',
-    changes => [
-      # Do pam_krb5 before pam_ssh because pam_krb5 is optional
-      "ins 01 after *[type = 'auth' and module = 'pam_krb5.so']",
-      "mv *[type = 'auth' and module = 'pam_ssh.so'] 01",
-
-      # pam_krb5 is optional
-      "setm *[(type = 'auth' or type = 'session') and module = 'pam_krb5.so'] control optional",
-
-      # pam_krb5 should authenticate with the principal listed in ~/.k5login
-      "rm *[type = 'auth' and module = 'pam_krb5.so']/argument",
-      "set *[type = 'auth' and module = 'pam_krb5.so']/argument search_k5login",
-
-      # pam_ssh should use the password that pam_krb5 asked for
-      "rm *[type = 'auth' and module = 'pam_ssh.so']/argument",
-      "set *[type = 'auth' and module = 'pam_ssh.so']/argument use_first_pass",
-
-      # pam_krb5 should not have any effect on account status
-      # and should not be involved in password changes
-      "rm *[(type = 'account' or type = 'password') and module = 'pam_krb5.so']",
-
-      # pam_krb5 session doesn't need any arguments
-      "rm *[type = 'session' and module = 'pam_krb5.so']/argument",
-    ],
-    require => Nest::Portage::Package_use['sys-auth/pambase'],
-  } 
 }
