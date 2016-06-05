f522e2dfa88a725d18841d8069516a8aa19f95ce
James T. Lee <james@james.tl>
2016-06-05 17:19:09 -0400

    zfs: Set vm.min_free_kbytes for better stability on low-mem systems

--- tmp/nest/manifests/profile/base/zfs.pp
+++ tmp/nest/manifests/profile/base/zfs.pp
@@ -7,4 +7,9 @@ class nest::profile::base::zfs {
     enable  => true,
     require => Package['sys-fs/zfs'],
   }
+
+  # ZFS likes to have a little extra headroom on low memory systems
+  sysctl { 'vm.min_free_kbytes':
+    value => '32768',
+  }
 }
