2f9d67f6bf42c56799b3e4fe40798f07f16763de
James T. Lee <james@james.tl>
2016-06-06 18:11:18 -0400

    distccd: Disable verbose mode

--- tmp/nest/manifests/profile/base/distccd.pp
+++ tmp/nest/manifests/profile/base/distccd.pp
@@ -1,15 +1,22 @@
 class nest::profile::base::distccd {
   if $::nest::distcc_server {
-    file_line { 'distccd-allowed-servers':
-      path  => '/etc/systemd/system/distccd.service.d/00gentoo.conf',
-      line  => 'Environment="ALLOWED_SERVERS=172.22.2.0/24"',
-      match => '^Environment="ALLOWED_SERVERS=',
+    $disable_verbose_content = @(EOT)
+      [Service]
+      ExecStart=
+      ExecStart=/usr/bin/distccd --no-detach --daemon --port 3632 -N 15 --allow 172.22.2.0/24
+      | EOT
+
+    file { '/etc/systemd/system/distccd.service.d/10-nest.conf':
+      mode    => '0644',
+      owner   => 'root',
+      group   => 'root',
+      content => $disable_verbose_content,
+      notify  => Exec['distccd-systemd-daemon-reload'],
     }
 
     exec { 'distccd-systemd-daemon-reload':
       command     => '/usr/bin/systemctl daemon-reload',
       refreshonly => true,
-      subscribe   => File_line['distccd-allowed-servers'],
       notify      => Service['distccd']
     }
 
