3df2378f46993cd619e02ff3f5fa0a7f14f955bf
James T. Lee <james@james.tl>
2016-06-09 18:00:55 -0400

    packages: Add gentoolkit

--- tmp/nest/manifests/profile/base/packages.pp
+++ tmp/nest/manifests/profile/base/packages.pp
@@ -1,6 +1,7 @@
 class nest::profile::base::packages {
   package { [
     'app-editors/vim',
+    'app-portage/gentoolkit',
     'net-dns/bind-tools',
     'sys-fs/dosfstools',
     'sys-block/parted',
