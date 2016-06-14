5aa129a680eae3b3557c46412b6a95761a5f67d8
James T. Lee <james@james.tl>
2016-06-13 22:37:13 -0400

    packages: Add additional utilities

--- tmp/nest/manifests/profile/base/packages.pp
+++ tmp/nest/manifests/profile/base/packages.pp
@@ -1,9 +1,13 @@
 class nest::profile::base::packages {
   package { [
+    'app-admin/sysstat',
     'app-editors/vim',
+    'app-misc/screen',
     'app-portage/gentoolkit',
+    'dev-libs/libisofs',
     'net-dns/bind-tools',
     'sys-fs/dosfstools',
+    'sys-fs/squashfs-tools',
     'sys-block/parted',
     'www-client/elinks',
   ]:
