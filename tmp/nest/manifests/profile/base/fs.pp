2507a73d435f2a5f9c615444e9513afa7f7e9c1e
James T. Lee <james@james.tl>
2016-06-15 21:09:09 -0400

    fs: Make /var/lib/nfs/v4recovery

--- tmp/nest/manifests/profile/base/fs.pp
+++ tmp/nest/manifests/profile/base/fs.pp
@@ -3,10 +3,38 @@ class nest::profile::base::fs {
     ensure => installed,
   }
 
+  file { '/etc/systemd/system/nfs-server.service.d':
+    ensure => directory,
+    mode   => '0655',
+    owner  => 'root',
+    group  => 'root',
+
+    # Not strictly required, but packages pull in systemd
+    require => Package['net-fs/nfs-utils'],
+  }
+
+  $nfs_server_make_v4recovery = @(EOT)
+    [Service]
+    ExecStartPre=-/bin/mkdir -p /var/lib/nfs/v4recovery
+    | EOT
+
+  file { '/etc/systemd/system/nfs-server.service.d/10-v4recovery.conf':
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    content => $nfs_server_make_v4recovery,
+    notify  => Exec['nfs-server-systemd-daemon-reload'],
+  }
+
+  exec { 'nfs-server-systemd-daemon-reload':
+    command     => '/usr/bin/systemctl daemon-reload',
+    refreshonly => true,
+  }
+
   if $::nest::fileserver {
     service { 'nfs-server':
-      enable  => true,
-      require => Package['net-fs/nfs-utils'],
+      enable    => true,
+      subscribe => Exec['nfs-server-systemd-daemon-reload'],
     }
 
     service { 'zfs-share':
