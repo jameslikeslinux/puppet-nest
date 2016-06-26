2bc3a3df22b08891421bb389e12f24881136e72f
James T. Lee <james@james.tl>
2016-06-26 19:03:16 -0400

    docker: Include docker class

--- tmp/nest/manifests/docker.pp
+++ tmp/nest/manifests/docker.pp
@@ -3,4 +3,9 @@ class nest::docker {
     name       => "${::trusted['certname']}/docker",
     mountpoint => '/var/lib/docker',
   }
+
+  class { 'docker':
+    service_provider => 'systemd',
+    require          => Zfs['docker'],
+  }
 }
