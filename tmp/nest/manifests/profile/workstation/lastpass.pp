546cf2af00e224806a30154257f985f45c7065fb
James T. Lee <james@james.tl>
2016-12-05 23:37:56 -0500

    lastpass: Move script to dot directory

--- tmp/nest/manifests/profile/workstation/lastpass.pp
+++ tmp/nest/manifests/profile/workstation/lastpass.pp
@@ -3,23 +3,30 @@ class nest::profile::workstation::lastpass {
     ensure => installed,
   }
 
-  exec { '/bin/mkdir -p /home/james/bin':
-    unless  => '/usr/bin/test -d /home/james/bin',
+  exec { '/bin/mkdir -p /home/james/.lastpass':
+    unless  => '/usr/bin/test -d /home/james/.lastpass',
     require => File['/home/james'],
   }
 
-  file { '/home/james/bin':
+  file { '/home/james/.lastpass':
     ensure  => directory,
     mode    => '0755',
     owner   => 'james',
     group   => 'users',
-    require => Exec['/bin/mkdir -p /home/james/bin'],
+    require => Exec['/bin/mkdir -p /home/james/.lastpass'],
   }
 
-  file { '/home/james/bin/lpass-copy':
+  file { '/home/james/.lastpass/lpass-copy':
     mode    => '0755',
     owner   => 'james',
     group   => 'users',
     content => template('nest/lastpass/lpass-copy.erb'),
-  } 
+  }
+
+  file { '/home/james/.lastpass/lastpass.khotkeys':
+    mode   => '0644',
+    owner  => 'james',
+    group  => 'users',
+    source => 'puppet:///modules/nest/lastpass/lastpass.khotkeys',
+  }
 }
