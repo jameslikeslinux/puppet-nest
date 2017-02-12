9356122dc45e3de437a9640eb82619358bb57c84
James T. Lee <james@james.tl>
2017-02-12 02:05:26 -0500

    nest: Update Breeze cursors

--- tmp/nest/manifests/init.pp
+++ tmp/nest/manifests/init.pp
@@ -67,7 +67,7 @@ class nest (
   $console_font_size_smaller = inline_template('<%= @console_font_sizes.reverse.find(16) { |size| size - @console_font_size_ideal.round <= 0 } %>')
   $console_font_size         = $console_font_size_smaller
 
-  $cursor_sizes        = [24, 32, 40, 48, 64, 96]
+  $cursor_sizes        = [24, 32, 36, 40, 48, 64, 96]
   $cursor_size_ideal   = 24 * $::nest::scaling_factor
   $cursor_size_smaller = inline_template('<%= @cursor_sizes.reverse.find(24) { |size| size - @cursor_size_ideal.round <= 0 } %>')
   $cursor_size         = $cursor_size_smaller
