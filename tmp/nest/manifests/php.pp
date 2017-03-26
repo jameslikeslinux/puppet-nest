35bde3339a46e91db2b9e03f21b8dc59a35b92ef
James T. Lee <james@james.tl>
2017-03-26 00:10:31 -0400

    php: eselect-php needs to use fpm

--- tmp/nest/manifests/php.pp
+++ tmp/nest/manifests/php.pp
@@ -1,10 +1,17 @@
 class nest::php {
-  nest::portage::package_use { 'dev-lang/php':
+  nest::portage::package_use { [
+    'app-eselect/eselect-php',
+    'dev-lang/php',
+  ]:
     use => 'fpm',
   }
 
   package { 'dev-lang/php':
-    ensure => installed,
+    ensure  => installed,
+
+    # The dependency on Nest::Portage::Package_use['dev-lang/php'] is implied;
+    # eselect-php needs to use fpm too
+    require => Nest::Portage::Package_use['app-eselect/eselect-php'],
   }
 
   service { 'php-fpm@7.0':
