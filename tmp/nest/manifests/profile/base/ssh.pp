2a951751f0e5b94807f5c880942e83ced2349525
James T. Lee <james@james.tl>
2017-07-18 00:13:26 -0400

    Run ssh-agent under systemd
    Based on
    https://wiki.archlinux.org/index.php/SSH_keys#Start_ssh-agent_with_systemd_user

--- tmp/nest/manifests/profile/base/ssh.pp
+++ tmp/nest/manifests/profile/base/ssh.pp
@@ -25,4 +25,29 @@ class nest::profile::base::ssh {
   service { 'sshd':
     enable => true,
   }
+
+  file { '/etc/systemd/user/ssh-agent.service':
+    mode   => '0644',
+    owner  => 'root',
+    group  => 'root',
+    source => 'puppet:///modules/nest/ssh/ssh-agent.service',
+    notify => Exec['ssh-systemd-daemon-reload'],
+  }
+
+  exec { 'ssh-systemd-daemon-reload':
+    command     => '/usr/bin/systemctl daemon-reload',
+    refreshonly => true,
+  }
+
+  exec { 'ssh-agent-enable-systemd-user-service':
+    command => '/usr/bin/systemctl --user --global enable ssh-agent.service',
+    creates => '/etc/systemd/user/default.target.wants/ssh-agent.service',
+    require => File['/etc/systemd/user/ssh-agent.service'],
+  }
+
+  file_line { 'pam_env.conf-SSH_AUTH_SOCK':
+    path  => '/etc/security/pam_env.conf',
+    line  => 'SSH_AUTH_SOCK	DEFAULT="${XDG_RUNTIME_DIR}/ssh-agent.socket"',
+    match => '^SSH_AUTH_SOCK\s',
+  }
 }
