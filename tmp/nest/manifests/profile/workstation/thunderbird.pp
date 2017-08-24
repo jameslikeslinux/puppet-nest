2ba563d93574f6f28098d1b1b94dab30811101a3
James T. Lee <james@james.tl>
2017-08-24 11:49:13 -0400

    nest: Remove thunderbird

--- tmp/nest/manifests/profile/workstation/thunderbird.pp
+++ tmp/nest/manifests/profile/workstation/thunderbird.pp
@@ -1,13 +1,10 @@
 class nest::profile::workstation::thunderbird {
   package { 'mail-client/thunderbird':
-    ensure => installed,
+    ensure => absent,
   }
 
   file { '/usr/lib/thunderbird/defaults/pref/all-nest.js':
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
-    content => "pref(\"layout.css.devPixelsPerPx\", \"${::nest::text_scaling_factor}\");\n",
-    require => Package['mail-client/thunderbird'],
+    ensure => absent,
+    before => Package['mail-client/thunderbird'],
   }
 }
