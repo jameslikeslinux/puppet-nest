5e366e8df84e358fbd70f48beb34ec11095b882f
James T. Lee <james@james.tl>
2017-08-24 00:20:59 -0400

    systemd: Fix bad file_line resource

--- tmp/nest/manifests/profile/base/systemd.pp
+++ tmp/nest/manifests/profile/base/systemd.pp
@@ -31,7 +31,7 @@ class nest::profile::base::systemd {
   file_line { 'locale.gen-en_US.UTF-8':
     path  => '/etc/locale.gen',
     line  => 'en_US.UTF-8 UTF-8',
-    match => '^#en_US.UTF-8 UTF-8$',
+    match => '^#?en_US.UTF-8 UTF-8$',
   }
 
   exec { '/usr/sbin/locale-gen':
