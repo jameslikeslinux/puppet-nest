a5c0647aa9b86e83db514c2cb145c206cb3ee469
James T. Lee <james@james.tl>
2017-09-29 21:35:10 -0400

    fonts: Remove "preference" related font configs
    They have been moved to my dotfiles.

--- tmp/nest/manifests/profile/workstation/fonts.pp
+++ tmp/nest/manifests/profile/workstation/fonts.pp
@@ -12,19 +12,28 @@ class nest::profile::workstation::fonts {
   }
 
   file { '/etc/fonts/local.conf':
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
-    source  => 'puppet:///modules/nest/fonts/local.conf',
-
-    # fontconfig is pulled in by the portage profile, and all packages
-    # depend on the portage profile, so this is just an easy way to
-    # establish that relationship.
-    require => Package['media-fonts/corefonts'],
+    ensure => absent,
   }
 
-  file { '/etc/fonts/conf.d/10-hinting-slight.conf':
-    ensure  => absent,
-    require => Package['media-fonts/corefonts'],
+  # The intention here is to express configurations that are related to the
+  # system, not necessarily my preference.  In this case, all of my systems are
+  # RGB LCDs; however, this could be made configurable in Hiera with some
+  # module parameters.  User preference type configurations, like hinting,
+  # belong in the user's home directory (~/.config/fontconfig/fonts.conf)
+  $font_confs = [
+    '10-sub-pixel-rgb.conf',
+    '11-lcdfilter-default.conf',
+  ]
+
+  $font_confs.each |$conf| {
+    file { "/etc/fonts/conf.d/${conf}":
+      ensure  => link,
+      target  => "../conf.avail/${conf}",
+
+      # fontconfig is pulled in by the portage profile, and all packages
+      # depend on the portage profile, so this is just an easy way to
+      # establish that relationship.
+      require => Package['media-fonts/corefonts'],
+    }
   }
 }
