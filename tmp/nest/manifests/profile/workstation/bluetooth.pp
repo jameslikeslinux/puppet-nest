160e0abf44505e402113dd22998c231b646ad97d
James T. Lee <james@james.tl>
2017-10-04 15:44:32 -0400

    bluetooth: Fetch firmware for Plugable dongle

--- tmp/nest/manifests/profile/workstation/bluetooth.pp
+++ tmp/nest/manifests/profile/workstation/bluetooth.pp
@@ -9,4 +9,11 @@ class nest::profile::workstation::bluetooth {
   service { 'bluetooth':
     enable => true,
   }
+
+  # http://plugable.com/2014/06/23/plugable-usb-bluetooth-adapter-solving-hfphsp-profile-issues-on-linux/
+  exec { 'wget-plugable-bluetooth-firmware':
+    command => '/usr/bin/wget https://s3.amazonaws.com/plugable/bin/fw-0a5c_21e8.hcd -O /lib/firmware/brcm/BCM20702A1-0a5c-21e8.hcd',
+    creates => '/lib/firmware/brcm/BCM20702A1-0a5c-21e8.hcd',
+    require => Package['sys-kernel/linux-firmware'],
+  }
 }
