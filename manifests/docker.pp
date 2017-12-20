dfe76afe870efb7fe2b4c6c7b359f120c7007077
James T. Lee <james@james.tl>
2017-12-20 16:16:38 -0500

    docker: Add more params for gentoo-systemd compatibility

--- manifests/docker.pp
+++ manifests/docker.pp
@@ -5,9 +5,12 @@ class nest::docker {
   }
 
   class { 'docker':
-    docker_ce_package_name => 'app-emulation/docker',
-    service_provider       => 'systemd',
-    require                => Zfs['docker'],
+    docker_ce_package_name     => 'app-emulation/docker',
+    service_config             => '/etc/conf.d/docker',
+    service_config_template    => 'docker/etc/sysconfig/docker.systemd.erb',
+    service_overrides_template => 'docker/etc/systemd/system/docker.service.d/service-overrides-archlinux.conf.erb',
+    service_provider           => 'systemd',
+    require                    => Zfs['docker'],
   }
 
   User <| title == 'james' |> {
