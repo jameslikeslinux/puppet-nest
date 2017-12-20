9a37939afa97322d0df241ab5606c4e7710fcf86
James T. Lee <james@james.tl>
2017-12-20 16:24:39 -0500

    docker: Fix systemd overrides

--- manifests/docker.pp
+++ manifests/docker.pp
@@ -8,7 +8,7 @@ class nest::docker {
     docker_ce_package_name     => 'app-emulation/docker',
     service_config             => '/etc/conf.d/docker',
     service_config_template    => 'docker/etc/sysconfig/docker.systemd.erb',
-    service_overrides_template => 'docker/etc/systemd/system/docker.service.d/service-overrides-archlinux.conf.erb',
+    service_overrides_template => 'nest/docker/systemd-overrides.conf.erb',
     service_provider           => 'systemd',
     require                    => Zfs['docker'],
   }
