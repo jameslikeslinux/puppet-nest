80a5b4ab35a4fbad49937afc66654b40b24883ac
James T. Lee <james@james.tl>
2018-06-07 12:24:07 -0400

    vhost: Support custom ports

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -2,6 +2,7 @@ define nest::vhost (
   String[1] $servername,
   Array[String[1]] $serveraliases                    = [],
   Optional[Variant[String[1], Array[String[1]]]] $ip = undef,
+  Optional[Integer] $port                            = undef,
   Boolean $ssl                                       = true,
   Hash[String[1], Any] $extra_params                 = {},
   Boolean $zfs_docroot                               = true,
@@ -9,44 +10,61 @@ define nest::vhost (
 ) {
   include '::nest::apache'
 
-  ensure_resource('apache::listen', '80', {})
+  $http_port = $port ? {
+    undef   => 80,
+    default => $port,
+  }
+
+  $https_port = $port ? {
+    undef   => 443,
+    default => $port,
+  }
+
+  $vhost_redirect_port = $port ? {
+    undef   => '',
+    default => ":${port}",
+  }
+
+  ensure_resource('apache::listen', $http_port, {})
 
   nest::srv { "www/${servername}":
     zfs => $zfs_docroot,
   }
 
   if $ssl {
-    ensure_resource('apache::listen', '443', {})
+    ensure_resource('apache::listen', $https_port, {})
 
-    $vhost_name   = "${servername}-ssl"
+    $vhost_name   = "${name}-ssl"
     $vhost_params = {
-      'port'     => '443',
+      'port'     => $https_port,
       'ssl'      => true,
       'ssl_cert' => "/etc/letsencrypt/live/${servername}/fullchain.pem",
       'ssl_key'  => "/etc/letsencrypt/live/${servername}/privkey.pem",
     }
-    $vhost_redirect_to = 'https'
+    $vhost_redirect_proto = 'https'
 
-    apache::vhost { "${servername}-redirect":
-      priority        => $priority,
-      servername      => $servername,
-      serveraliases   => $serveraliases,
-      ip              => $ip,
-      port            => '80',
-      add_listen      => false,
-      docroot         => "/srv/www/${servername}",
-      docroot_owner   => 'james',
-      docroot_group   => 'users',
-      redirect_status => 'permanent',
-      redirect_dest   => "https://${servername}/",
-      require         => Nest::Srv["www/${servername}"],
+    unless $port {
+      apache::vhost { "${name}-redirect":
+        priority        => $priority,
+        servername      => $servername,
+        serveraliases   => $serveraliases,
+        ip              => $ip,
+        port            => '80',
+        add_listen      => false,
+        docroot         => "/srv/www/${servername}",
+        docroot_owner   => 'james',
+        docroot_group   => 'users',
+        redirect_status => 'permanent',
+        redirect_dest   => "https://${servername}/",
+        require         => Nest::Srv["www/${servername}"],
+      }
     }
   } else {
-    $vhost_name   = $servername
+    $vhost_name   = $name
     $vhost_params = {
-      'port' => '80'
+      'port' => $http_port,
     }
-    $vhost_redirect_to = 'http'
+    $vhost_redirect_proto = 'http'
   }
 
   apache::vhost { $vhost_name:
@@ -72,7 +90,7 @@ define nest::vhost (
       docroot_owner   => 'james',
       docroot_group   => 'users',
       redirect_status => 'permanent',
-      redirect_dest   => "${vhost_redirect_to}://${servername}/",
+      redirect_dest   => "${vhost_redirect_proto}://${servername}${vhost_redirect_port}/",
       require         => Nest::Srv["www/${servername}"],
       *               => $vhost_params,
     }
