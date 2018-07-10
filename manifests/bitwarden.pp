a2323be6cc652876e635f5819583dab073cf2ff1
James T. Lee <james@james.tl>
2018-07-10 02:43:36 -0400

    bitwarden: Add mssql volume to /etc/fstab

--- manifests/bitwarden.pp
+++ manifests/bitwarden.pp
@@ -11,6 +11,19 @@ class nest::bitwarden (
 
   nest::srv { 'bitwarden': }
 
+  # A hack to include my manually-created ext4 volume for MSSQL in /etc/fstab
+  Augeas <| title == 'fstab' |> {
+    changes +> [
+      "set 99/spec /dev/zvol/${hostname}/srv/bitwarden/mssql",
+      'set 99/file /srv/bitwarden/bwdata/mssql',
+      'set 99/vfstype ext4',
+      'set 99/opt[1] defaults',
+      'set 99/opt[2] discard',
+      'set 99/dump 0',
+      'set 99/passno 0',
+    ]
+  }
+
   file { '/srv/bitwarden':
     ensure  => directory,
     mode    => '0750',
