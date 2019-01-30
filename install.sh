f960c00b5720cdc8e9bba57152a1fca284e18143
James T. Lee <james@james.tl>
2019-01-30 11:57:41 -0500

    install: Avoid conflict with 'command' built-in

--- install.sh
+++ install.sh
@@ -90,22 +90,22 @@ cleanup() {
     task "Cleaning up..."
 
     # Unmount if necessary
-    mountpoint -q "/mnt/${name}" && command umount -R "/mnt/${name}"
+    mountpoint -q "/mnt/${name}" && cmd umount -R "/mnt/${name}"
 
     # Remove mountpoint if necessary
     if [ -e "/mnt/${name}" ] && [ ! "$(ls -A "/mnt/${name}")" ]; then
-        command rm -rf "/mnt/${name}"
+        cmd rm -rf "/mnt/${name}"
     fi
 
     # Disable swap if necessary (assume its on if the device exists)
-    [ -e "/dev/zvol/${name}/swap" ] && command swapoff "/dev/zvol/${name}/swap"
+    [ -e "/dev/zvol/${name}/swap" ] && cmd swapoff "/dev/zvol/${name}/swap"
 
     # Export pool filesystem if necessary
-    zpool list "$name" > /dev/null 2>&1 && command zpool export "$name"
+    zpool list "$name" > /dev/null 2>&1 && cmd zpool export "$name"
 
     # Close LUKS devices if necessary
     for vdev in "${vdevs[@]}"; do
-        [ -e "/dev/mapper/${vdev}" ] && command cryptsetup luksClose "$vdev"
+        [ -e "/dev/mapper/${vdev}" ] && cmd cryptsetup luksClose "$vdev"
     done
 
     trap - EXIT
@@ -119,7 +119,7 @@ END
 }
 
 
-command() {
+cmd() {
     echo "> $@" | tee -a "$LOGFILE"
     if [ -z "$dryrun" ]; then
         "$@" >> "$LOGFILE" 2>&1
@@ -131,7 +131,7 @@ command() {
 }
 
 
-chroot_command() {
+chroot_cmd() {
     echo ">> $@" | tee -a "$LOGFILE"
     if [ -z "$dryrun" ]; then
         FACTER_chroot=true chroot "/mnt/${name}" "$@" >> "$LOGFILE" 2>&1
@@ -173,22 +173,22 @@ fi
 
 trap cleanup EXIT
 task "Making build target..."
-command mkdir "/mnt/${name}"
+cmd mkdir "/mnt/${name}"
 
 
 if [ -n "$live" ]; then
     live_dir="${PWD}/${name}"
 
     task "Making live CD directory structure..."
-    command mkdir -p "${live_dir}/LiveOS/squashfs-root/LiveOS"
+    cmd mkdir -p "${live_dir}/LiveOS/squashfs-root/LiveOS"
 
     task "Making live CD root image..."
-    command truncate -s 10G "${live_dir}/LiveOS/squashfs-root/LiveOS/rootfs.img"
-    command mkfs.ext4 "${live_dir}/LiveOS/squashfs-root/LiveOS/rootfs.img"
-    command tune2fs -o discard "${live_dir}/LiveOS/squashfs-root/LiveOS/rootfs.img"
+    cmd truncate -s 10G "${live_dir}/LiveOS/squashfs-root/LiveOS/rootfs.img"
+    cmd mkfs.ext4 "${live_dir}/LiveOS/squashfs-root/LiveOS/rootfs.img"
+    cmd tune2fs -o discard "${live_dir}/LiveOS/squashfs-root/LiveOS/rootfs.img"
 
     task "Mounting image at build target..."
-    command mount "${live_dir}/LiveOS/squashfs-root/LiveOS/rootfs.img" "/mnt/${name}"
+    cmd mount "${live_dir}/LiveOS/squashfs-root/LiveOS/rootfs.img" "/mnt/${name}"
 else
     disk_number=0
     if [ "${#disks[@]}" -gt 1 ]; then
@@ -214,7 +214,7 @@ else
 
     for disk in "${disks[@]}"; do
         task "Partitioning ${disk}..."
-        command parted -s "$disk" \
+        cmd parted -s "$disk" \
             mklabel gpt \
             mkpart "${name}-${partition_name_suffix}${mirror_number}" 1MiB $((1 + 32))MiB \
             set 1 "$partition_flag" on \
@@ -224,17 +224,17 @@ else
             unit s \
             print
 
-        command udevadm trigger
+        cmd udevadm trigger
 
         if [ -n "$efi" ]; then
             task "Making EFI system partition ${name}-${partition_name_suffix}${mirror_number}..."
-            command mkfs.vfat "/dev/disk/by-partlabel/${name}-${partition_name_suffix}${mirror_number}"
+            cmd mkfs.vfat "/dev/disk/by-partlabel/${name}-${partition_name_suffix}${mirror_number}"
         fi
 
         if [ -n "$encrypt" ]; then
             task "Encrypting ${name}-crypt${disk_number}..."
-            echo -n "$enc_passphrase" | command cryptsetup luksFormat -c aes-xts-plain64 -s 256 -h sha512 "/dev/disk/by-partlabel/${name}${partition_name_crypt}${disk_number}"
-            echo -n "$enc_passphrase" | command cryptsetup luksOpen "/dev/disk/by-partlabel/${name}${partition_name_crypt}${disk_number}" "${name}${partition_name_crypt}${disk_number}"
+            echo -n "$enc_passphrase" | cmd cryptsetup luksFormat -c aes-xts-plain64 -s 256 -h sha512 "/dev/disk/by-partlabel/${name}${partition_name_crypt}${disk_number}"
+            echo -n "$enc_passphrase" | cmd cryptsetup luksOpen "/dev/disk/by-partlabel/${name}${partition_name_crypt}${disk_number}" "${name}${partition_name_crypt}${disk_number}"
         fi
 
         vdevs+=("${name}${partition_name_crypt}${disk_number}")
@@ -244,51 +244,51 @@ else
     done
 
     task "Creating ZFS pool..."
-    command zpool create -f -m none -O compression=lz4 -O xattr=sa -O acltype=posixacl -R "/mnt/${name}" "$name" "${vdevs[@]}"
-    command zfs create "${name}/ROOT"
-    command zfs create -o mountpoint=/ "${name}/ROOT/gentoo"
-    command zfs create -o mountpoint=/var "${name}/ROOT/gentoo/var"
-    command zfs create -o mountpoint=/home "${name}/home"
-    command zfs create "${name}/home/james"
-    command zpool set bootfs="${name}/ROOT/gentoo" "$name"
+    cmd zpool create -f -m none -O compression=lz4 -O xattr=sa -O acltype=posixacl -R "/mnt/${name}" "$name" "${vdevs[@]}"
+    cmd zfs create "${name}/ROOT"
+    cmd zfs create -o mountpoint=/ "${name}/ROOT/gentoo"
+    cmd zfs create -o mountpoint=/var "${name}/ROOT/gentoo/var"
+    cmd zfs create -o mountpoint=/home "${name}/home"
+    cmd zfs create "${name}/home/james"
+    cmd zpool set bootfs="${name}/ROOT/gentoo" "$name"
 
     task "Creating swap space..."
-    command zfs create -V 2G -b $(getconf PAGESIZE) -o com.sun:auto-snapshot=false "${name}/swap"
-    command mkswap "/dev/zvol/${name}/swap"
-    command swapon --discard "/dev/zvol/${name}/swap"
+    cmd zfs create -V 2G -b $(getconf PAGESIZE) -o com.sun:auto-snapshot=false "${name}/swap"
+    cmd mkswap "/dev/zvol/${name}/swap"
+    cmd swapon --discard "/dev/zvol/${name}/swap"
 
     task "Creating fscache..."
-    command zfs create -V 2G -b 4k -o com.sun:auto-snapshot=false "${name}/fscache"
-    command mkfs.ext4 "/dev/zvol/${name}/fscache"
-    command tune2fs -o discard "/dev/zvol/${name}/fscache"
+    cmd zfs create -V 2G -b 4k -o com.sun:auto-snapshot=false "${name}/fscache"
+    cmd mkfs.ext4 "/dev/zvol/${name}/fscache"
+    cmd tune2fs -o discard "/dev/zvol/${name}/fscache"
 
     # XXX: This needs to support mdraid
     task "Creating boot filesystem..."
-    command mkfs.ext2 -L "${name}-boot" "/dev/disk/by-partlabel/${name}-boot"
-    command mkdir "/mnt/${name}/boot"
-    command mount LABEL="${name}-boot" "/mnt/${name}/boot"
+    cmd mkfs.ext2 -L "${name}-boot" "/dev/disk/by-partlabel/${name}-boot"
+    cmd mkdir "/mnt/${name}/boot"
+    cmd mount LABEL="${name}-boot" "/mnt/${name}/boot"
 fi
 
 
 task "Downloading and extracting stage archive..."
-command wget --progress=dot:mega "$STAGE_ARCHIVE" -O "/mnt/${name}/$(basename "$STAGE_ARCHIVE")"
-command tar -C "/mnt/${name}" -xvjpf "/mnt/${name}/$(basename "$STAGE_ARCHIVE")" --xattrs
+cmd wget --progress=dot:mega "$STAGE_ARCHIVE" -O "/mnt/${name}/$(basename "$STAGE_ARCHIVE")"
+cmd tar -C "/mnt/${name}" -xvjpf "/mnt/${name}/$(basename "$STAGE_ARCHIVE")" --xattrs
 
 
 task "Initializing chroot..."
-command mount -t proc proc "/mnt/${name}/proc"
-command mount --rbind /sys "/mnt/${name}/sys"
-command mount --make-rslave "/mnt/${name}/sys"
-command mount --rbind /dev "/mnt/${name}/dev"
-command mount --make-rslave "/mnt/${name}/dev"
-command mkdir "/mnt/${name}/nest"
-command mount --rbind /nest "/mnt/${name}/nest"
-command mount --make-rslave "/mnt/${name}/nest"
-command cp -L /etc/resolv.conf "/mnt/${name}/etc/resolv.conf"
+cmd mount -t proc proc "/mnt/${name}/proc"
+cmd mount --rbind /sys "/mnt/${name}/sys"
+cmd mount --make-rslave "/mnt/${name}/sys"
+cmd mount --rbind /dev "/mnt/${name}/dev"
+cmd mount --make-rslave "/mnt/${name}/dev"
+cmd mkdir "/mnt/${name}/nest"
+cmd mount --rbind /nest "/mnt/${name}/nest"
+cmd mount --make-rslave "/mnt/${name}/nest"
+cmd cp -L /etc/resolv.conf "/mnt/${name}/etc/resolv.conf"
 
 
 task "Prepping build target..."
-command tee "/mnt/${name}/etc/portage/make.conf" <<END
+cmd tee "/mnt/${name}/etc/portage/make.conf" <<END
 CFLAGS="-march=haswell -O2 -pipe -ggdb"
 CXXFLAGS="-march=haswell -O2 -pipe -ggdb"
 CPU_FLAGS_X86="aes avx avx2 fma3 mmx mmxext popcnt sse sse2 sse3 sse4_1 sse4_2 ssse3"
@@ -297,63 +297,63 @@ EMERGE_DEFAULT_OPTS="\${EMERGE_DEFAULT_OPTS} --usepkg"
 FEATURES="buildpkg splitdebug"
 PKGDIR="/nest/portage/packages/amd64-base"
 END
-command tee "/mnt/${name}/etc/portage/repos.conf" <<END
+cmd tee "/mnt/${name}/etc/portage/repos.conf" <<END
 [DEFAULT]
 main-repo = gentoo
 
 [gentoo]
 location = /var/cache/portage/gentoo
 END
-command rm -rf "/mnt/${name}/etc/portage/make.conf.catalyst"
-command rm -rf "/mnt/${name}/etc/portage/package."*
+cmd rm -rf "/mnt/${name}/etc/portage/make.conf.catalyst"
+cmd rm -rf "/mnt/${name}/etc/portage/package."*
 
 
 task "Installing Portage tree..."
-command wget --progress=dot:mega https://github.com/iamjamestl/portage-gentoo/archive/master.tar.gz -O "/mnt/${name}/portage-gentoo-master.tar.gz"
-command mkdir -p "/mnt/${name}/var/cache/portage/gentoo"
-command tar -C "/mnt/${name}/var/cache/portage/gentoo" --strip 1 -xvzf "/mnt/${name}/portage-gentoo-master.tar.gz"
-command rm -f "/mnt/${name}/portage-gentoo-master.tar.gz"
-chroot_command eselect profile set 1
+cmd wget --progress=dot:mega https://github.com/iamjamestl/portage-gentoo/archive/master.tar.gz -O "/mnt/${name}/portage-gentoo-master.tar.gz"
+cmd mkdir -p "/mnt/${name}/var/cache/portage/gentoo"
+cmd tar -C "/mnt/${name}/var/cache/portage/gentoo" --strip 1 -xvzf "/mnt/${name}/portage-gentoo-master.tar.gz"
+cmd rm -f "/mnt/${name}/portage-gentoo-master.tar.gz"
+chroot_cmd eselect profile set 1
 
 
 task "Installing Puppet..."
-chroot_command emerge -v app-admin/puppet-agent app-portage/eix
+chroot_cmd emerge -v app-admin/puppet-agent app-portage/eix
 
 
 task "Prepping for Puppet run..."
-chroot_command mkdir -p /etc/puppetlabs/facter/facts.d
-chroot_command tee /etc/puppetlabs/facter/facts.d/nest.yaml <<END
+chroot_cmd mkdir -p /etc/puppetlabs/facter/facts.d
+chroot_cmd tee /etc/puppetlabs/facter/facts.d/nest.yaml <<END
 ---
 nest:
   profile: '${profile}'
 END
-[ -n "$live" ] && chroot_command tee -a /etc/puppetlabs/facter/facts.d/nest.yaml <<END
+[ -n "$live" ] && chroot_cmd tee -a /etc/puppetlabs/facter/facts.d/nest.yaml <<END
   live: true
 END
 
 
 task "Running Puppet..."
-chroot_command puppet agent --onetime --verbose --no-daemonize --no-splay --show_diff --certname "$name" --server puppet.nest
-[ -z "$live" ] && chroot_command systemctl enable puppet
+chroot_cmd puppet agent --onetime --verbose --no-daemonize --no-splay --show_diff --certname "$name" --server puppet.nest
+[ -z "$live" ] && chroot_cmd systemctl enable puppet
 
 task "Removing unnecessary packages..."
-chroot_command emerge --depclean
+chroot_cmd emerge --depclean
 
 
 if [ -n "$live" ]; then
     iso_label=$(echo "$name" | tr 'a-z' 'A-Z')
 
     task "Configuring live CD boot..."
-    command mkdir "${live_dir}/boot"
-    command mv "/mnt/${name}/boot/"* "${live_dir}/boot/"
-    command sed -i -r '/insmod ext2/,/fi/d; s/root=UUID=[[:graph:]]+/root=live:LABEL='"$iso_label"'/g' "${live_dir}/boot/grub/grub.cfg"
+    cmd mkdir "${live_dir}/boot"
+    cmd mv "/mnt/${name}/boot/"* "${live_dir}/boot/"
+    cmd sed -i -r '/insmod ext2/,/fi/d; s/root=UUID=[[:graph:]]+/root=live:LABEL='"$iso_label"'/g' "${live_dir}/boot/grub/grub.cfg"
 
     cleanup
 
     task "Making squashfs.img..."
-    command mksquashfs "${live_dir}/LiveOS/squashfs-root" "${live_dir}/LiveOS/squashfs.img" -comp xz
-    command rm -rf "${live_dir}/LiveOS/squashfs-root"
+    cmd mksquashfs "${live_dir}/LiveOS/squashfs-root" "${live_dir}/LiveOS/squashfs.img" -comp xz
+    cmd rm -rf "${live_dir}/LiveOS/squashfs-root"
 
     task "Making ISO..."
-    command grub-mkrescue --modules=part_gpt -o "${name}-${DATE}.iso" "$live_dir" -- -volid "$iso_label"
+    cmd grub-mkrescue --modules=part_gpt -o "${name}-${DATE}.iso" "$live_dir" -- -volid "$iso_label"
 fi
