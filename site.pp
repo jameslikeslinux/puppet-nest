8203c7b5e0b16f254e4e4bb47b6107ff78cf13a3
James T. Lee <james@james.tl>
2019-02-04 11:20:44 -0500

    site: Ensure chocolatey is installed on Windows

--- site.pp
+++ site.pp
@@ -10,4 +10,14 @@ Firewallchain {
   noop => str2bool("$::chroot")
 }
 
+if $facts['kernel'] == 'windows' {
+  stage { 'first':
+    before => Stage['main'],
+  }
+
+  class { 'chocolatey':
+    stage => 'first',
+  }
+}
+
 hiera_include('classes')
