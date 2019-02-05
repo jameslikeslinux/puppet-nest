24ed7bcf28ea8d6661a73cffa47a06690d887658
James T. Lee <james@james.tl>
2019-02-05 16:12:31 -0500

    git: Use cygpath wrapper

--- manifests/profile/base/git.pp
+++ manifests/profile/base/git.pp
@@ -17,16 +17,27 @@ class nest::profile::base::git {
         provider => 'cygwin',
       }
 
+      $git_win_wrapper_content = @(END_GIT_WIN_WRAPPER)
+        #!/bin/bash
+        cygpath -- "$@" | xargs git
+        | END_GIT_WIN_WRAPPER
+
+      file { 'C:/tools/cygwin/usr/local/bin/git-win':
+        mode    => '0755',
+        content => $git_win_wrapper_content,
+        require => Package['git'],
+      }
+
       $git_batch_content = @(END_GIT_BAT)
         @echo off
         setlocal
         set PATH=C:/tools/cygwin/bin
-        C:/tools/cygwin/bin/git.exe %*
+        C:/tools/cygwin/bin/bash C:/tools/cygwin/usr/local/bin/git-win %*
         | END_GIT_BAT
 
       file { 'C:/Program Files/Puppet Labs/Puppet/bin/git.bat':
         content => $git_batch_content,
-        require => Package['git'],
+        require => File['C:/tools/cygwin/usr/local/bin/git-win'],
       }
     }
   }
