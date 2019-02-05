eb16358b56978cf7dee28103bc62a0fc9daecd09
James T. Lee <james@james.tl>
2019-02-05 03:05:16 -0500

    cygwin: Fix perms on postinstall

--- manifests/profile/base/cygwin.pp
+++ manifests/profile/base/cygwin.pp
@@ -2,4 +2,21 @@ class nest::profile::base::cygwin {
   package { 'cygwin':
     ensure => installed,
   }
+
+  $fix_perms_content = @(END_FIX_PERMS)
+    find "$(cygpath -am /)" -not -path "$(cygpath -am /home)*" -exec chown -h Administrators {} +
+    | END_FIX_PERMS
+
+  file { 'C:/tools/cygwin/etc/postinstall/zzz-fix-perms.sh':
+    content => $fix_perms_content,
+    notify  => Exec['cygwin-fix-perms'],
+  }
+
+  exec { 'cygwin-fix-perms':
+    command     => shellquote(
+      'C:/tools/cygwin/bin/bash.exe', '-c',
+      'source /etc/profile && source /etc/postinstall/zzz-fix-perms.sh'
+    ),
+    refreshonly => true,
+  }
 }
