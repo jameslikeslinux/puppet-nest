ca2c980216a6c63f6c02526f09d4d2a3d897f5b9
James T. Lee <james@james.tl>
2019-02-05 13:34:30 -0500

    cygwin_home_perms: Fix subshell syntax

--- manifests/cygwin_home_perms.pp
+++ manifests/cygwin_home_perms.pp
@@ -4,34 +4,16 @@ define nest::cygwin_home_perms (
   $user_quoted = shellquote($user)
 
   $find_bad_ownership = "find ~${user_quoted} -not \( -user ${user_quoted} -group Administrators \)"
-  $ownership_ok = shellquote(
-    'C:/tools/cygwin/bin/bash.exe', '-c',
-    "source /etc/profile && output=\"\$(${find_bad_ownership})\" && [[ \$output == '' ]]"
-  )
-  $fix_ownership = shellquote(
-    'C:/tools/cygwin/bin/bash.exe', '-c',
-    "source /etc/profile && ${find_bad_ownership} -exec chown -h ${user_quoted}:Administrators {} +"
-  )
+  $ownership_ok = "C:/tools/cygwin/bin/bash.exe -c 'source /etc/profile && output=\"\$(${find_bad_ownership})\" && [[ \$output == \"\" ]]'"
+  $fix_ownership = "C:/tools/cygwin/bin/bash.exe -c 'source /etc/profile && ${find_bad_ownership} -exec chown -h ${user_quoted}:Administrators {} +'"
 
   $find_bad_group_perms = "find ~${user_quoted} -not -perm -g=w"
-  $group_perms_ok = shellquote(
-    'C:/tools/cygwin/bin/bash.exe', '-c',
-    "source /etc/profile && output=\"\$(${find_bad_group_perms})\" && [[ \$output == '' ]]"
-  )
-  $fix_group_perms = shellquote(
-    'C:/tools/cygwin/bin/bash.exe', '-c',
-    "source /etc/profile && ${find_bad_group_perms} -exec chmod g+w {} +"
-  )
+  $group_perms_ok = "C:/tools/cygwin/bin/bash.exe -c 'source /etc/profile && output=\"\$(${find_bad_group_perms})\" && [[ \$output == \"\" ]]'"
+  $fix_group_perms = "C:/tools/cygwin/bin/bash.exe -c 'source /etc/profile && ${find_bad_group_perms} -exec chmod g+w {} +'"
 
   $find_bad_setgid_dirs = "find ~${user_quoted} -type d -not -perm -g=s"
-  $setgid_dirs_ok = shellquote(
-    'C:/tools/cygwin/bin/bash.exe', '-c',
-    "source /etc/profile && output=\"\$(${find_bad_setgid_dirs})\" && [[ \$output == '' ]]"
-  )
-  $fix_setgid_dirs = shellquote(
-    'C:/tools/cygwin/bin/bash.exe', '-c',
-    "source /etc/profile && ${find_bad_setgid_dirs} -exec chmod g+s {} +"
-  )
+  $setgid_dirs_ok = "C:/tools/cygwin/bin/bash.exe -c 'source /etc/profile && output=\"\$(${find_bad_setgid_dirs})\" && [[ \$output == \"\" ]]'"
+  $fix_setgid_dirs = "C:/tools/cygwin/bin/bash.exe -c 'source /etc/profile && ${find_bad_setgid_dirs} -exec chmod g+s {} +'"
 
   exec {
     "fix-cygwin-home-ownership-${title}":
