15519e8d4add3c45dec5e5bb29eca31ea135887a
James T. Lee <james@james.tl>
2019-02-16 17:55:16 -0500

    unifi-video: Use plain-old docker volumes

--- manifests/unifi_video.pp
+++ manifests/unifi_video.pp
@@ -2,36 +2,6 @@ class nest::unifi_video {
   include '::nest'
   include '::nest::docker'
 
-  nest::srv { 'unifi-video': }
-
-  file {
-    default:
-      owner  => 'ubnt',
-      group  => 'ubnt',
-    ;
-
-    '/srv/unifi-video':
-      ensure  => directory,
-      mode    => '0775',
-      require => Nest::Srv['unifi-video'],
-    ;
-
-    '/srv/unifi-video/system.properties':
-      ensure  => file,
-      mode    => '0664',
-      content => "is_default=true\n",
-      replace => false,
-    ;
-  }
-
-  file_line { 'unifi-video-app.http.port':
-    path    => '/srv/unifi-video/system.properties',
-    line    => 'app.http.port=80',
-    match   => '^app\.http\.port=',
-    require => File['/srv/unifi-video/system.properties'],
-    notify  => Docker::Run['unifi-video'],
-  }
-
   docker_network { 'video':
     ensure  => present,
     driver  => 'macvlan',
@@ -40,21 +10,17 @@ class nest::unifi_video {
     options => "parent=bond0.1003",
   }
 
+  docker_volume { 'unifi-video':
+    ensure => present,
+  }
+
   $cpuset = $::nest::availcpus_expanded.join(',')
 
   docker::run { 'unifi-video':
-    image            => 'unifi-video-controller',
+    image            => 'unifi-video',
     net              => 'video',
     dns              => '172.22.3.1',
-    env              => [
-      'PUID=1002',
-      'PGID=1002',
-      'TZ=America/New_York',
-    ],
-    volumes          => [
-      '/srv/unifi-video:/var/lib/unifi-video',
-      '/nest/cameras:/var/lib/unifi-video/videos',
-    ],
+    volumes          => 'unifi-video:/var/lib/unifi-video',
     extra_parameters => [
       "--cpuset-cpus=${cpuset}",
       '--ip=172.22.3.2',
@@ -65,7 +31,7 @@ class nest::unifi_video {
     service_provider => 'systemd',
     require          => [
       Docker_network['video'],
-      File['/srv/unifi-video'],
+      Docker_volume['unifi-video'],
     ],
   }
 }
