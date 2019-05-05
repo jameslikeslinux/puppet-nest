cf3d68469517e6c177ed95edc9f2ea944f624529
James T. Lee <james@james.tl>
2019-05-05 04:38:10 -0400

    media: Remove gpmdp in favor of chromium

--- manifests/profile/workstation/media.pp
+++ manifests/profile/workstation/media.pp
@@ -1,18 +1,5 @@
 class nest::profile::workstation::media {
   package { 'media-sound/google-play-music-desktop-player-bin':
-    ensure => installed,
-  }
-
-  $gpmdp_wrapper = @(GPMDP_WRAPPER)
-    #!/bin/bash
-    exec "/usr/share/google-play-music-desktop-player/Google Play Music Desktop Player" --disable-smooth-scrolling --enable-gpu "$@"
-    | GPMDP_WRAPPER
-
-  file { '/usr/bin/google-play-music-desktop-player':
-    mode    => '0755',
-    owner   => 'root',
-    group   => 'root',
-    content => $gpmdp_wrapper,
-    require => Package['media-sound/google-play-music-desktop-player-bin'],
+    ensure => absent,
   }
 }
