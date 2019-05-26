f5dc5ad133610d344cfcfd3ca881e755ac747fdd
James T. Lee <james@james.tl>
2019-05-26 18:11:58 -0400

    tlp: Set DISK_DEVICES

--- manifests/profile/workstation/tlp.pp
+++ manifests/profile/workstation/tlp.pp
@@ -8,11 +8,21 @@ class nest::profile::workstation::tlp {
     ensure => installed,
   }
 
-  file_line { 'tlp.conf-TLP_ENABLE':
-    path    => '/etc/tlp.conf',
-    line    => 'TLP_ENABLE=1',
-    match   => '#?TLP_ENABLE=',
-    require => Package['app-laptop/tlp'],
+  file_line {
+    default:
+      path    => '/etc/tlp.conf',
+      require => Package['app-laptop/tlp'],
+    ;
+
+    'tlp.conf-TLP_ENABLE':
+      line  => 'TLP_ENABLE=1',
+      match => '#?TLP_ENABLE=',
+    ;
+
+    'tlp.conf-DISK_DEVICES':
+      line  => "DISK_DEVICES=\"${facts['disks'].keys.join(' ')}\"",
+      match => '#?DISK_DEVICES=',
+    ;
   }
 
   exec {
