1238eef6586db091bb5b77f05cf7895f5452572d
James T. Lee <james@james.tl>
2019-05-29 21:35:04 -0400

    Improve rpool fact

--- lib/facter/rpool.rb
+++ lib/facter/rpool.rb
@@ -1,10 +1,14 @@
 Facter.add('rpool') do
   confine :kernel => 'Linux'
   setcode do
-    certname = Facter::Core::Execution.execute('puppet agent --configprint certname').gsub(/\..*/, '')
-    ["#{certname}/crypt", certname].each do |name|
-      Facter::Core::Execution.execute("zfs list #{name}")
-      break name if $? == 0
+    hostname = Facter.value('hostname')
+    if File.exist?('/sbin/zfs')
+      ["#{hostname}/crypt", hostname].each do |name|
+        Facter::Core::Execution.execute("/sbin/zfs list #{name}")
+        break name if $? == 0
+      end
+    else
+      hostname
     end
   end
 end
