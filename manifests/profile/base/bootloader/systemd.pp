6de86eb028b699f041ecde79cd6d10cd3d6e625e
James T. Lee <james@james.tl>
2020-01-09 00:04:41 -0500

    systemd-boot: Assume /efi is mounted

--- manifests/profile/base/bootloader/systemd.pp
+++ manifests/profile/base/bootloader/systemd.pp
@@ -6,19 +6,10 @@ class nest::profile::base::bootloader::systemd {
       exec { "bootctl-install-${disk}":
         command => '/usr/bin/bootctl install',
         creates => '/efi/EFI/systemd/systemd-bootx64.efi',
-        require => Exec["mount-${disk}"],
-        notify  => Exec["unmount-${disk}"],
       }
 
       exec { "bootctl-update-${disk}":
         command     => '/usr/bin/bootctl update',
-        refreshonly => true,
-        require     => Exec["mount-${disk}"],
-        notify      => Exec["unmount-${disk}"],
-      }
-
-      exec { "unmount-${disk}":
-        command     => '/bin/umount /efi && rm -rf /efi',
         refreshonly => true,
       }
     }
