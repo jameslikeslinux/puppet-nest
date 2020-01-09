15654d521272976407fc01db284f6752ec8598b5
James T. Lee <james@james.tl>
2020-01-09 02:53:26 -0500

    fstab: Mount /boot and /efi for systemd-boot systems

--- manifests/profile/base/fstab.pp
+++ manifests/profile/base/fstab.pp
@@ -1,11 +1,6 @@
 class nest::profile::base::fstab {
   $hostname = regsubst($::trusted['certname'], '\..*', '')
 
-  $boot_vfstype = $::nest::bootloader ? {
-    systemd => 'vfat',
-    default => 'ext2',
-  }
-
   # XXX: Improve this
   if $::nest::live {
     $base_changes = [
@@ -28,53 +23,68 @@ class nest::profile::base::fstab {
       'rm *[spec]',
 
       $::nest::bootloader ? {
-        systemd => [],
+        systemd => [
+          "set 1/spec PARTLABEL=${hostname}-boot",
+          'set 1/file /boot',
+          "set 1/vfstype vfat",
+          'set 1/opt defaults',
+          'set 1/dump 0',
+          'set 1/passno 2',
+
+          "set 2/spec PARTLABEL=${hostname}-efi",
+          'set 2/file /efi',
+          "set 2/vfstype vfat",
+          'set 2/opt defaults',
+          'set 2/dump 0',
+          'set 2/passno 2',
+        ],
+
         default => [
           "set 1/spec LABEL=${hostname}-boot",
           'set 1/file /boot',
-          "set 1/vfstype ${boot_vfstype}",
+          "set 1/vfstype ext2",
           'set 1/opt defaults',
           'set 1/dump 0',
           'set 1/passno 2',
         ],
       },
 
-      "set 2/spec LABEL=${hostname}-swap",
-      'set 2/file none',
-      'set 2/vfstype swap',
-      'set 2/opt discard',
-      'set 2/dump 0',
-      'set 2/passno 0',
-
-      'set 3/spec none',
-      'set 3/file /var',
-      'set 3/vfstype none',
-      'set 3/opt[1] fake',
-      'set 3/opt[2] x-systemd.requires',
-      'set 3/opt[2]/value zfs-mount.service',
+      "set 3/spec LABEL=${hostname}-swap",
+      'set 3/file none',
+      'set 3/vfstype swap',
+      'set 3/opt discard',
       'set 3/dump 0',
       'set 3/passno 0',
+
+      'set 4/spec none',
+      'set 4/file /var',
+      'set 4/vfstype none',
+      'set 4/opt[1] fake',
+      'set 4/opt[2] x-systemd.requires',
+      'set 4/opt[2]/value zfs-mount.service',
+      'set 4/dump 0',
+      'set 4/passno 0',
     ].flatten
 
     $nfs_changes = [
-      "set 4/spec LABEL=${hostname}-fscache",
-      'set 4/file /var/cache/fscache',
-      'set 4/vfstype ext4',
-      'set 4/opt[1] defaults',
-      'set 4/opt[2] discard',
-      'set 4/dump 0',
-      'set 4/passno 0',
+      "set 10/spec LABEL=${hostname}-fscache",
+      'set 10/file /var/cache/fscache',
+      'set 10/vfstype ext4',
+      'set 10/opt[1] defaults',
+      'set 10/opt[2] discard',
+      'set 10/dump 0',
+      'set 10/passno 0',
 
-      "set 5/spec ${::nest::nestfs_hostname}:/nest",
-      'set 5/file /nest',
-      'set 5/vfstype nfs',
-      'set 5/opt[1] noauto',
-      'set 5/opt[2] fsc',
-      'set 5/opt[3] x-systemd.automount',
-      'set 5/opt[4] x-systemd.requires',
-      'set 5/opt[4]/value openvpn-client@nest.service',
-      'set 5/dump 0',
-      'set 5/passno 0',
+      "set 11/spec ${::nest::nestfs_hostname}:/nest",
+      'set 11/file /nest',
+      'set 11/vfstype nfs',
+      'set 11/opt[1] noauto',
+      'set 11/opt[2] fsc',
+      'set 11/opt[3] x-systemd.automount',
+      'set 11/opt[4] x-systemd.requires',
+      'set 11/opt[4]/value openvpn-client@nest.service',
+      'set 11/dump 0',
+      'set 11/passno 0',
     ]
   }
 
