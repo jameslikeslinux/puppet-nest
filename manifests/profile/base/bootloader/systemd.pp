4ce50336b788cdf970f94d5bfd9c4359cdda99e6
James T. Lee <james@james.tl>
2020-01-08 23:57:08 -0500

    systemd-boot: Fix dependencies

--- manifests/profile/base/bootloader/systemd.pp
+++ manifests/profile/base/bootloader/systemd.pp
@@ -11,20 +11,14 @@ class nest::profile::base::bootloader::systemd {
       exec { "bootctl-install-${disk}":
         command => '/usr/bin/bootctl install',
         creates => '/efi/EFI/systemd/systemd-bootx64.efi',
-        require => [
-          Package['sys-boot/systemd-boot'],
-          Exec["mount-${disk}"],
-        ],
+        require => Exec["mount-${disk}"],
         notify  => Exec["unmount-${disk}"],
       }
 
       exec { "bootctl-update-${disk}":
         command     => '/usr/bin/bootctl update',
         refreshonly => true,
-        require     => [
-          Package['sys-boot/systemd-boot'],
-          Exec["mount-${disk}"],
-        ],
+        require     => Exec["mount-${disk}"],
         notify      => Exec["unmount-${disk}"],
       }
 
