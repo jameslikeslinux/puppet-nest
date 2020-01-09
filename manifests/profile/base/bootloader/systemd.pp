e58ad1ade39d9696ecff7b0ee520b27e77bb9a1b
James T. Lee <james@james.tl>
2020-01-09 00:06:08 -0500

    systemd-boot: Simplify conditional

--- manifests/profile/base/bootloader/systemd.pp
+++ manifests/profile/base/bootloader/systemd.pp
@@ -2,7 +2,7 @@ class nest::profile::base::bootloader::systemd {
   $::partitions.each |$partition, $attributes| {
     $disk = regsubst($partition, 'p?(art)?\d+$', '')
 
-    if "${::trusted['certname']}-" in $attributes['partlabel'] and $attributes['partlabel'] =~ /-efi$/ {
+    if $attributes['partlabel'] == "${::trusted['certname']}-efi" {
       exec { "bootctl-install-${disk}":
         command => '/usr/bin/bootctl install',
         creates => '/efi/EFI/systemd/systemd-bootx64.efi',
