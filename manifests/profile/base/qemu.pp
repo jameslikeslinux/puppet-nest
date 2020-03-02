5149cd35f2ec7e891b00dc78fea2b19baa593065
James T. Lee <james@james.tl>
2020-03-02 11:41:14 -0500

    Revert "qemu: Enable qemu-arm on distcc server"
    This reverts commit 57b001a47af3f3244dcd9e12526eb26682f6819f.

--- manifests/profile/base/qemu.pp
+++ manifests/profile/base/qemu.pp
@@ -9,21 +9,6 @@ class nest::profile::base::qemu {
       package { 'app-emulation/qemu-guest-agent':
         ensure => $qemu_guest_agent_ensure,
       }
-
-      if $::nest::distcc_server {
-        nest::portage::package_use { [
-          'dev-libs/glib',
-          'sys-libs/zlib',
-          'sys-apps/attr',
-          'dev-libs/libpcre',
-        ]:
-          use => 'static-libs',
-        }
-
-        Nest::Portage::Package_use <| title == 'app-emulation/qemu' |> {
-          use +> ['static-user', 'qemu_user_targets_arm'],
-        }
-      }
     }
 
     'windows': {
