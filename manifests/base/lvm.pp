d5601fcbc90c3fe7d5bc021d20e474e5c69238a3
James T. Lee <james@james.tl>
2020-04-16 16:37:34 -0400

    lvm: Restore configuration
    lvm2 gets pulled in by libvirt so we have to deal with it.

--- manifests/base/lvm.pp
+++ manifests/base/lvm.pp
@@ -1,5 +1,12 @@
 class nest::base::lvm {
   package { 'sys-fs/lvm2':
-    ensure => absent,
+    ensure => installed,
+  }
+
+  file_line { 'lvm.conf-global_filter-zvol':
+    path    => '/etc/lvm/lvm.conf',
+    line    => "\tglobal_filter = [ \"r|/dev/zd.*|\" ]",
+    match   => 'global_filter = ',
+    require => Package['sys-fs/lvm2'],
   }
 }
