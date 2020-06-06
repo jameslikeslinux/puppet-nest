bcbb5573e08859cf8f92ceed1064c79eb711d73f
James T. Lee <james@james.tl>
2020-06-06 19:42:07 -0400

    systemd-boot: Fix path to aarch64 image

--- manifests/base/bootloader/systemd.pp
+++ manifests/base/bootloader/systemd.pp
@@ -36,7 +36,7 @@ class nest::base::bootloader::systemd {
   $image = $facts['os']['architecture'] ? {
     'amd64'   => '/usr/src/linux/arch/x86/boot/bzImage',
     'armv7l'  => '/usr/src/linux/arch/arm/boot/zImage',
-    'aarch64' => '/usr/src/linux/arch/arm64/boot/zImage',
+    'aarch64' => '/usr/src/linux/arch/arm64/boot/Image.gz',
   }
 
   exec { 'kernel-install':
