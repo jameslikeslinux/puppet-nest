d47b1f833834c6530685233d60c5af648b06c92f
James T. Lee <james@james.tl>
2020-06-15 01:56:16 -0400

    fstab: Simplify logic by defining canned fstabs

--- manifests/base/fstab.pp
+++ manifests/base/fstab.pp
@@ -1,106 +1,40 @@
 class nest::base::fstab {
   $hostname = regsubst($::trusted['certname'], '\..*', '')
 
-  # XXX: Improve this
-  if $facts['live'] {
-    $base_changes = [
-      'rm *[spec]',
-    ]
+  $boot_vfstype = $::nest::bootloader ? {
+    'systemd' => 'vfat',
+    default   => 'ext2',
+  }
 
-    $nfs_changes = [
-      "set 1/spec ${::nest::nestfs_hostname}:/nest",
-      'set 1/file /nest',
-      'set 1/vfstype nfs',
-      'set 1/opt[1] noauto',
-      'set 1/opt[2] x-systemd.automount',
-      'set 1/opt[3] x-systemd.requires',
-      'set 1/opt[3]/value openvpn-client@nest.service',
+  $specs = {
+    'boot'    => [
+      "set 1/spec PARTLABEL=${hostname}-boot",
+      'set 1/file /boot',
+      "set 1/vfstype ${boot_vfstype}",
+      'set 1/opt defaults',
       'set 1/dump 0',
-      'set 1/passno 0',
-    ]
-  } else {
-    case $::platform {
-      'beagleboneblack': {
-        $efi = []
-        $fscache = []
-      }
+      'set 1/passno 2',
+    ],
 
-      'pinebookpro': {
-        $efi = []
+    'efi'     => [
+      "set 2/spec PARTLABEL=${hostname}-efi",
+      'set 2/file /efi',
+      "set 2/vfstype vfat",
+      'set 2/opt defaults',
+      'set 2/dump 0',
+      'set 2/passno 2',
+    ],
 
-        $fscache = [
-          'set 10/opt[4] fsc',
-          'set 10/opt[5] x-systemd.requires',
-          'set 10/opt[5]/value cachefilesd.service',
-
-          "set 11/spec LABEL=${hostname}-fscache",
-          'set 11/file /var/cache/fscache',
-          'set 11/vfstype ext4',
-          'set 11/opt[1] defaults',
-          'set 11/opt[2] discard',
-          'set 11/dump 0',
-          'set 11/passno 0',
-        ]
-      }
-
-      default: {
-        $efi = [
-          "set 2/spec PARTLABEL=${hostname}-efi",
-          'set 2/file /efi',
-          "set 2/vfstype vfat",
-          'set 2/opt defaults',
-          'set 2/dump 0',
-          'set 2/passno 2',
-        ]
-
-        $fscache = [
-          'set 10/opt[4] fsc',
-          'set 10/opt[5] x-systemd.requires',
-          'set 10/opt[5]/value cachefilesd.service',
-
-          "set 11/spec LABEL=${hostname}-fscache",
-          'set 11/file /var/cache/fscache',
-          'set 11/vfstype ext4',
-          'set 11/opt[1] defaults',
-          'set 11/opt[2] discard',
-          'set 11/dump 0',
-          'set 11/passno 0',
-        ]
-      }
-    }
-
-    $base_changes = [
-      'rm *[spec]',
-
-      $::nest::bootloader ? {
-        systemd => [
-          "set 1/spec PARTLABEL=${hostname}-boot",
-          'set 1/file /boot',
-          "set 1/vfstype vfat",
-          'set 1/opt defaults',
-          'set 1/dump 0',
-          'set 1/passno 2',
-
-          $efi,
-        ],
-
-        default => [
-          "set 1/spec LABEL=${hostname}-boot",
-          'set 1/file /boot',
-          "set 1/vfstype ext2",
-          'set 1/opt defaults',
-          'set 1/dump 0',
-          'set 1/passno 2',
-        ],
-      },
-
+    'swap'    => [
       "set 3/spec LABEL=${hostname}-swap",
       'set 3/file none',
       'set 3/vfstype swap',
       'set 3/opt discard',
       'set 3/dump 0',
       'set 3/passno 0',
+    ],
 
+    'var'     => [
       'set 4/spec none',
       'set 4/file /var',
       'set 4/vfstype none',
@@ -109,31 +43,61 @@ class nest::base::fstab {
       'set 4/opt[2]/value zfs-mount.service',
       'set 4/dump 0',
       'set 4/passno 0',
-    ].flatten
+    ],
 
-    $nfs_changes = [
-      "set 10/spec ${::nest::nestfs_hostname}:/nest",
-      'set 10/file /nest',
-      'set 10/vfstype nfs',
-      'set 10/opt[1] noauto',
-      'set 10/opt[2] x-systemd.automount',
-      'set 10/opt[3] x-systemd.requires',
-      'set 10/opt[3]/value openvpn-client@nest.service',
-      'set 10/dump 0',
-      'set 10/passno 0',
+    'nest-fscache' => [
+      "set 5/spec LABEL=${hostname}-fscache",
+      'set 5/file /var/cache/fscache',
+      'set 5/vfstype ext4',
+      'set 5/opt[1] defaults',
+      'set 5/opt[2] discard',
+      'set 5/dump 0',
+      'set 5/passno 0',
 
-      $fscache
-    ].flatten
+      "set 6/spec ${::nest::nestfs_hostname}:/nest",
+      'set 6/file /nest',
+      'set 6/vfstype nfs',
+      'set 6/opt[1] noauto',
+      'set 6/opt[2] fsc',
+      'set 6/opt[3] x-systemd.automount',
+      'set 6/opt[4] x-systemd.requires',
+      'set 6/opt[4]/value openvpn-client@nest.service',
+      'set 6/opt[5] x-systemd.requires',
+      'set 6/opt[5]/value cachefilesd.service',
+      'set 6/dump 0',
+      'set 6/passno 0',
+    ],
+
+    'nest-nocache' => [
+      "set 6/spec ${::nest::nestfs_hostname}:/nest",
+      'set 6/file /nest',
+      'set 6/vfstype nfs',
+      'set 6/opt[1] noauto',
+      'set 6/opt[2] x-systemd.automount',
+      'set 6/opt[3] x-systemd.requires',
+      'set 6/opt[3]/value openvpn-client@nest.service',
+      'set 6/dump 0',
+      'set 6/passno 0',
+    ],
   }
 
-  $changes = $::nest::nestfs_hostname ? {
-    "${hostname}.nest" => $base_changes,
-    default            => $base_changes + $nfs_changes,
+  if $facts['live'] {
+    $fstab = $specs['nest-nocache']
+  } elsif $::platform == 'beagleboneblack' {
+    $fstab = $specs['boot'] + $specs['swap'] + $specs['var'] + $specs['nest-nocache']
+  } elsif $::platform == 'pinebookpro' {
+    $fstab = $specs['boot'] + $specs['swap'] + $specs['var'] + $specs['nest-fscache']
+  } elsif $::nest::nestfs_hostname == "${hostname}.nest" {
+    $fstab = $specs['boot'] + $specs['efi'] + $specs['swap'] + $specs['var']
+  } elsif $::nest::bootloader == 'systemd' {
+    $fstab = $specs['boot'] + $specs['efi'] + $specs['swap'] + $specs['var'] + $specs['nest-fscache']
+  } else {
+    $fstab = $specs['boot'] + $specs['swap'] + $specs['var'] + $specs['nest-fscache']
   }
 
   augeas { 'fstab':
     context => '/files/etc/fstab',
-    changes => $changes,
+    changes => ['rm *[spec]'] + $fstab
   }
 
   # XXX: Hide harmless error at shutdown when trying to unmount /var due to
