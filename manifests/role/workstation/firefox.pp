35891826447db3b2f800dd45b53c0389ee146e36
James T. Lee <james@james.tl>
2020-06-23 17:55:12 -0400

    firefox: Enable nest.contextMenuOffsetTweak in wrapper

--- manifests/role/workstation/firefox.pp
+++ manifests/role/workstation/firefox.pp
@@ -17,13 +17,9 @@ class nest::role::workstation::firefox {
         ensure => installed,
       }
 
-      file { '/usr/lib64/firefox/defaults/pref/all-nest.js':
+      file { '/usr/lib64/firefox/defaults/pref/all-scaling.js':
         ensure => absent,
       }
-
-      $scaling_prefs = "pref(\"layout.css.devPixelsPerPx\", \"${::nest::gui_scaling_factor}\");"
-      $scaling_prefs_quoted = shellquote($scaling_prefs)
-      $scaling_prefs_echo_cmd = shellquote("echo ${scaling_prefs_quoted} > /usr/lib64/firefox/defaults/pref/all-scaling.js")
 
       $webrender = $::nest::video_card ? {
         'intel'  => 1,
@@ -31,31 +27,11 @@ class nest::role::workstation::firefox {
         default  => 0,
       }
 
-      $xdg_session_type = '$XDG_SESSION_TYPE'
-      $moz_disable_wayland = '$MOZ_DISABLE_WAYLAND'
-      $firefox_wrapper_content = @("EOT")
-        #!/bin/bash
-
-        if [[ $xdg_session_type == 'x11' ]]; then
-            sudo sh -c $scaling_prefs_echo_cmd
-            export GTK_USE_PORTAL=1 MOZ_USE_XINPUT2=1
-        else
-            sudo rm -f /usr/lib64/firefox/defaults/pref/all-scaling.js
-            if [[ $moz_disable_wayland == '1' ]]; then
-                unset GDK_DPI_SCALE
-            else
-                export MOZ_ENABLE_WAYLAND=1
-            fi
-        fi
-
-        MOZ_WEBRENDER=${webrender} MOZ_DBUS_REMOTE=1 exec /usr/lib64/firefox/firefox "$@"
-        | EOT
-
       file { '/usr/bin/firefox':
         mode    => '0755',
         owner   => 'root',
         group   => 'root',
-        content => $firefox_wrapper_content,
+        content => template('nest/firefox/wrapper.erb'),
         require => Package['www-client/firefox'],
       }
     }
