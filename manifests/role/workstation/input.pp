7208596735fed63a5d62698c5d93d248d9eaf6c7
James T. Lee <james@james.tl>
2020-08-13 12:00:02 -0400

    input: Deploy trackpad boundary config for pinebookpro

--- manifests/role/workstation/input.pp
+++ manifests/role/workstation/input.pp
@@ -1,4 +1,18 @@
 class nest::role::workstation::input {
+  $hwdb_ensure = $::nest::platform ? {
+    'pinebookpro' => present,
+    default       => absent,
+  }
+
+  $evdev_hwdb = @(EVDEV_HWDB)
+    # Pinebook Pro
+    evdev:input:b0003v258Ap001E*
+     EVDEV_ABS_00=5:1395:15
+     EVDEV_ABS_01=8:911:15
+     EVDEV_ABS_35=5:1395:15
+     EVDEV_ABS_36=8:911:15
+    | EVDEV_HWDB
+
   $keyboard_hwdb = @(KEYBOARD_HWDB)
     # Pinebook Pro
     evdev:input:b0003v258Ap001E*
@@ -7,17 +21,27 @@ class nest::role::workstation::input {
      KEYBOARD_KEY_70066=sleep
     | KEYBOARD_HWDB
 
-  file { '/etc/udev/hwdb.d/61-keyboard-local.hwdb':
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
-    content => $keyboard_hwdb,
-    notify  => Exec['udev-hwdb-update'],
-  }
+  file {
+    default:
+      mode    => '0644',
+      owner   => 'root',
+      group   => 'root',
+      notify  => Exec['udev-hwdb-update'],
+    ;
 
-  file { '/etc/udev/hwdb.d/71-mouse-local.hwdb':
-    ensure => absent,
-    notify => Exec['udev-hwdb-update'],
+    '/etc/udev/hwdb.d/61-evdev-local.hwdb':
+      ensure  => $hwdb_ensure,
+      content => $evdev_hwdb,
+    ;
+
+    '/etc/udev/hwdb.d/61-keyboard-local.hwdb':
+      ensure  => $hwdb_ensure,
+      content => $keyboard_hwdb,
+    ;
+
+    '/etc/udev/hwdb.d/71-mouse-local.hwdb':
+      ensure => absent,
+    ;
   }
 
   exec { 'udev-hwdb-update':
