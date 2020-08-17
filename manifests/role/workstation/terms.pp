8fca5a1238f33213c69d1a8d49bb29b0fc4f3465
James T. Lee <james@james.tl>
2020-08-16 21:16:41 -0400

    workstation: urxvt -> terms; install alacritty

--- manifests/role/workstation/terms.pp
+++ manifests/role/workstation/terms.pp
@@ -1,9 +1,10 @@
-class nest::role::workstation::urxvt {
+class nest::role::workstation::terms {
   nest::lib::portage::package_use { 'x11-terms/rxvt-unicode':
     use => ['256-color', 'alt-font-width', 'secondary-wheel', 'unicode3', '-vanilla', 'xft'],
   }
 
   package { [
+    'x11-terms/alacritty',
     'x11-terms/rxvt-unicode',
     'x11-misc/urxvt-font-size',
     'x11-misc/urxvt-perls',
