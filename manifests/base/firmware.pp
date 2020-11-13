2fdea5e0f75fcdfb607bae688339c5765b3b1df3
James T. Lee <james@james.tl>
2020-11-13 18:16:14 -0500

    firmware: Separate pinebookpro and raspberrypi files

--- manifests/base/firmware.pp
+++ manifests/base/firmware.pp
@@ -1,45 +1,12 @@
 class nest::base::firmware {
-  $files = $::platform ? {
-    'pinebookpro' => [
-      'brcm/BCM4345C5.hcd',
-      'brcm/brcmfmac43456-sdio.bin',
-      'brcm/brcmfmac43456-sdio.clm_blob',
-      'brcm/brcmfmac43456-sdio.txt',
-    ],
-
-    'raspberrypi' => [
-      'brcm/brcmfmac43456-sdio.bin',
-      'brcm/brcmfmac43456-sdio.clm_blob',
-      'brcm/brcmfmac43456-sdio.txt',
-    ],
-
-    default       => [],
-  }
-
-  $dirs = $files.reduce(['/lib/firmware']) |$acc, $file| {
-    $subdir = dirname($file)
-    $dir = "/lib/firmware/${subdir}"
-
-    if $subdir != '.' and !($dir in $acc) {
-      $acc + $dir
-    } else {
-      $acc
-    }
-  }
-
-  file { $dirs:
-    ensure => directory,
-    mode   => '0755',
-    owner  => 'root',
-    group  => 'root',
-  }
-
-  $files.each |$file| {
-    file { "/lib/firmware/${file}":
-      mode   => '0644',
-      owner  => 'root',
-      group  => 'root',
-      source => "puppet:///modules/nest/firmware/${file}",
+  if $::platform in ['pinebookpro', 'raspberrypi'] {
+    file { '/lib/firmware':
+      ensure  => 'directory',
+      mode    => '0644',
+      owner   => 'root',
+      group   => 'root',
+      recurse => true,
+      source  => "puppet:///modules/nest/firmware/${::platform}",
     }
   }
 }
