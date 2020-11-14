229e0080c18df6280ee62eb7ee47ad93a707ce3b
James T. Lee <james@james.tl>
2020-11-13 22:36:38 -0500

    Revert "install.sh: Tune swap volume for better stability"
    Not needed after zswap tuning.
    
    This reverts commit 0db1f5f31c6ea448b950e4d1f21478c60e3f4c5b.

--- install.sh
+++ install.sh
@@ -277,8 +277,7 @@ END
     fi
 
     task "Creating swap space..."
-    # See: https://github.com/openzfs/zfs/issues/7734
-    destructive_cmd zfs create -V 4G -b $(getconf PAGESIZE) -o sync=always -o primarycache=metadata -o secondarycache=none -o logbias=throughput "${zroot}/swap"
+    destructive_cmd zfs create -V 4G -b $(getconf PAGESIZE) "${zroot}/swap"
     destructive_cmd udevadm trigger
     destructive_cmd sleep 3
     destructive_cmd mkswap -L "${name}-swap" "/dev/zvol/${zroot}/swap"
