7192a3787887c95d31d4cf6c7ac74719d449af16
James T. Lee <james@james.tl>
2020-11-19 22:21:59 -0500

    firmware: Standardize on most up-to-date stable versions
    WiFi firmware on Pinebook Pro kept crashing, so lets try the version
    from Raspberry Pi OS again now that we're on the latest mainline kernel.
    The bluetooth firmware comes armbian, which has the most up-to-date
    version that seems to work on both Pinebook Pro and Raspberry Pi.  The
    version from Raspberry Pi OS doesn't work on Pinebook Pro.

--- manifests/base/firmware.pp
+++ manifests/base/firmware.pp
@@ -1,12 +1,10 @@
 class nest::base::firmware {
-  if $::platform in ['pinebookpro', 'raspberrypi'] {
-    file { '/lib/firmware':
-      ensure  => 'directory',
-      mode    => '0644',
-      owner   => 'root',
-      group   => 'root',
-      recurse => true,
-      source  => "puppet:///modules/nest/firmware/${::platform}",
-    }
+  file { '/lib/firmware':
+    ensure  => 'directory',
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    recurse => true,
+    source  => 'puppet:///modules/nest/firmware',
   }
 }
