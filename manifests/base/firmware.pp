edaa8996f5b55e368fa539cc8ab3c96afc9d2764
James T. Lee <james@james.tl>
2020-11-22 11:31:33 -0500

    firmware: Try to sort out platforms once and for all
    The bluetooth firmware from Manjaro doesn't work on raspberrypi and the
    firmware from Raspberry Pi OS doesn't work on pinebookpro.  So we will
    need to maintain the same firmware from different sources for different
    platforms.
    
    Meanwhile, I also need to clean up from having distributed the same
    firmware to all systems.

--- manifests/base/firmware.pp
+++ manifests/base/firmware.pp
@@ -3,12 +3,47 @@ class nest::base::firmware {
     ensure => installed,
   }
 
-  file { '/lib/firmware':
-    ensure  => 'directory',
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
-    recurse => true,
-    source  => 'puppet:///modules/nest/firmware',
+  $files = {
+    'manjaro/brcm/BCM4345C5.hcd'                   => ['pinebookpro'],
+    'plugable/brcm/BCM20702A1-0a5c-21e8.hcd'       => ['generic'],
+    'raspberrypi/brcm/BCM4345C5.hcd'               => ['raspberrypi'],
+    'raspberrypi/brcm/brcmfmac43456-sdio.bin'      => ['pinebookpro', 'raspberrypi'],
+    'raspberrypi/brcm/brcmfmac43456-sdio.clm_blob' => ['pinebookpro', 'raspberrypi'],
+    'raspberrypi/brcm/brcmfmac43456-sdio.txt'      => ['pinebookpro', 'raspberrypi'],
+  }
+
+  $files_categorized = $files.reduce([{}, {}]) |$memo, $file| {
+    $present   = $memo[0]
+    $absent    = $memo[1]
+    $source    = $file[0]
+    $platforms = $file[1]
+    $target    = regsubst($file[0], '^[^/]*/', '')
+
+    if $::platform in $platforms {
+      [$present + { $target => $source }, $absent]
+    } else {
+      [$present, $absent + { $target => $source }]
+    }
+  }
+
+  $files_present = $files_categorized[0]
+  $files_absent  = $files_categorized[1] - $files_categorized[0]
+
+  $files_present.each |$target, $source| {
+    file { "/lib/firmware/${target}":
+      mode    => '0644',
+      owner   => 'root',
+      group   => 'root',
+      source  => "puppet:///modules/nest/firmware/${source}",
+
+      # Makes the directory structure
+      require => Package['sys-kernel/linux-firmware'],
+    }
+  }
+
+  $files_absent.each |$target, $source| {
+    file { "/lib/firmware/${target}":
+      ensure => absent,
+    }
   }
 }
