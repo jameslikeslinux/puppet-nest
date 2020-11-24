f76dbbcea4c02065c32ce5bf0c079f60d5adeb3d
James T. Lee <james@james.tl>
2020-11-24 00:50:14 -0500

    bluetooth: Enable serial Bluetooth controller
    Ensure the serial Bluetooth controller is loaded as early on in the boot
    process as possible, like a traditional USB or other serial protocol
    controller would be by the kernel+udev.

--- manifests/role/workstation/bluetooth.pp
+++ manifests/role/workstation/bluetooth.pp
@@ -9,4 +9,21 @@ class nest::role::workstation::bluetooth {
   service { 'bluetooth':
     enable => true,
   }
+
+  if $::platform == 'raspberrypi' {
+    file { '/etc/systemd/system/btattach.service':
+      mode   => '0644',
+      owner  => 'root',
+      group  => 'root',
+      source => 'puppet:///modules/nest/bluetooth/btattach.service',
+      notify => Nest::Lib::Systemd_reload['bluetooth'],
+    }
+
+    ::nest::lib::systemd_reload { 'bluetooth': }
+
+    service { 'btattach':
+      enable  => true,
+      require => Nest::Lib::Systemd_reload['bluetooth'],
+    }
+  }
 }
