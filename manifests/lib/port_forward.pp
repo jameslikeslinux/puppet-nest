66ff02246cfb6e354f0a143c0f28acfc9206a22f
James T. Lee <james@james.tl>
2020-11-28 15:23:22 -0500

    port_forward: Combine IPv4 and IPv6 rule specifications

--- manifests/lib/port_forward.pp
+++ manifests/lib/port_forward.pp
@@ -6,75 +6,55 @@ define nest::lib::port_forward (
   Optional[Stdlib::IP::Address::V6] $source_ip6,
   Optional[Stdlib::IP::Address::V6] $destination_ip6,
 ) {
-  if $source_ip4 and $destination_ip4 {
-    firewall {
-      default:
-        provider => iptables,
-      ;
+  $combined_spec = {
+    'v4' => {
+      'source'      => $source_ip4,
+      'destination' => $destination_ip4,
+      'provider'    => iptables,
+    },
 
-      "100 ${name} dnat (v4)":
-        table       => nat,
-        chain       => 'PREROUTING',
-        destination => $source_ip4,
-        proto       => $proto,
-        dport       => $port,
-        jump        => 'DNAT',
-        todest      => $destination_ip4,
-      ;
-
-      "100 ${name} (v4)":
-        chain       => 'FORWARD',
-        destination => $destination_ip4,
-        proto       => $proto,
-        dport       => $port,
-        action      => accept,
-      ;
-
-      "100 ${name} snat (v4)":
-        table       => nat,
-        chain       => 'POSTROUTING',
-        destination => $destination_ip4,
-        proto       => $proto,
-        sport       => $port,
-        jump        => 'SNAT',
-        tosource    => $source_ip4,
-      ;
-    }
+    'v6' => {
+      'source'      => $source_ip6,
+      'destination' => $destination_ip6,
+      'provider'    => ip6tables,
+    },
   }
 
-  if $source_ip6 and $destination_ip6 {
-    firewall {
-      default:
-        provider => ip6tables,
-      ;
+  $combined_spec.each |$comment, $spec| {
+    if $spec['source'] and $spec['destination'] {
+      firewall {
+        default:
+          provider => $spec['provider'],
+        ;
 
-      "100 ${name} dnat (v6)":
-        table       => nat,
-        chain       => 'PREROUTING',
-        destination => $source_ip6,
-        proto       => $proto,
-        dport       => $port,
-        jump        => 'DNAT',
-        todest      => $destination_ip6,
-      ;
+        "100 ${name} dnat (${comment})":
+          table       => nat,
+          chain       => 'PREROUTING',
+          destination => $spec['source'],
+          proto       => $proto,
+          dport       => $port,
+          jump        => 'DNAT',
+          todest      => $spec['destination'],
+        ;
 
-      "100 ${name} (v6)":
-        chain       => 'FORWARD',
-        destination => $destination_ip6,
-        proto       => $proto,
-        dport       => $port,
-        action      => accept,
-      ;
+        "100 ${name} (${comment})":
+          chain       => 'FORWARD',
+          destination => $spec['destination'],
+          proto       => $proto,
+          dport       => $port,
+          action      => accept,
+        ;
 
-      "100 ${name} snat (v6)":
-        table       => nat,
-        chain       => 'POSTROUTING',
-        destination => $destination_ip6,
-        proto       => $proto,
-        sport       => $port,
-        jump        => 'SNAT',
-        tosource    => $source_ip6,
-      ;
+        "100 ${name} snat (${comment})":
+          table       => nat,
+          chain       => 'POSTROUTING',
+          destination => $spec['destination'],
+          proto       => $proto,
+          sport       => $port,
+          jump        => 'SNAT',
+          tosource    => $spec['source'],
+        ;
+      }
     }
   }
 }
