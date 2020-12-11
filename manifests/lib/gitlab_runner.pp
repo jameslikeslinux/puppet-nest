f03297018584b2c135b63fd69f03d784871b0910
James T. Lee <james@james.tl>
2020-12-10 21:18:29 -0500

    gitlab-runner: Add device support for buildah

--- manifests/lib/gitlab_runner.pp
+++ manifests/lib/gitlab_runner.pp
@@ -1,59 +1,75 @@
 define nest::lib::gitlab_runner (
   String $registration_token,
+  Enum['running', 'enabled', 'present', 'disabled', 'stopped', 'absent'] $ensure = running,
   String $host            = $name,
-  String $default_image   = 'ubuntu:latest',
-  String $description     = $facts['fqdn'],
+  String $default_image   = 'nest/gentoo/stage3:amd64-systemd',
+  Array[String] $devices  = [],
   Array[String] $volumes  = [],
   Array[String] $tag_list = [],
 ) {
-  # Required for /usr/bin/podman
-  include 'nest'
+  if $ensure == absent {
+    nest::lib::container { "gitlab-runner-${name}":
+      image  => 'gitlab/gitlab-runner',
+      ensure => absent,
+    }
+    ->
+    file { "/srv/gitlab-runner/${name}":
+      ensure  => absent,
+      recurse => true,
+      force   => true,
+    }
+  } else {
+    # Required for /usr/bin/podman
+    include 'nest'
 
-  # Required for /srv/gitlab-runner
-  include 'nest::service::gitlab_runner'
+    # Required for /srv/gitlab-runner
+    include 'nest::service::gitlab_runner'
 
-  file { "/srv/gitlab-runner/${name}":
-    ensure => directory,
-    mode   => '0755',
-    owner  => 'root',
-    group  => 'root',
-  }
+    file { "/srv/gitlab-runner/${name}":
+      ensure => directory,
+      mode   => '0755',
+      owner  => 'root',
+      group  => 'root',
+    }
 
-  $description_real = $description ? {
-    undef   => $facts['fqdn'],
-    default => "${facts['fqdn']}-${description}",
-  }
+    $device_args = $devices.map |$device| {
+      ['--docker-devices', $device]
+    }
 
-  $volume_args = $volumes.map |$volume| {
-    ['--docker-volumes', $volume]
-  }.flatten
+    $volume_args = $volumes.map |$volume| {
+      ['--docker-volumes', $volume]
+    }
 
-  # See: https://docs.gitlab.com/runner/register/index.html#one-line-registration-command
-  $register_command = [
-    '/usr/bin/podman', 'run', '--rm',
-    '-v', "/srv/gitlab-runner/${name}:/etc/gitlab-runner",
-    'gitlab/gitlab-runner', 'register',
-    '--non-interactive',
-    '--executor', 'docker',
-    '--docker-image', $default_image,
-    '--url', "https://${host}/",
-    '--registration-token', $registration_token,
-    '--description', $description,
-    '--tag-list', $tag_list.join(','),
-  ] + $volume_args
+    # See: https://docs.gitlab.com/runner/register/index.html#one-line-registration-command
+    $register_command = [
+      '/usr/bin/podman', 'run', '--rm',
+      '-v', "/srv/gitlab-runner/${name}:/etc/gitlab-runner",
+      'gitlab/gitlab-runner', 'register',
+      '--non-interactive',
+      '--executor', 'docker',
+      '--docker-image', $default_image,
+      $device_args,
+      $volume_args,
+      '--url', "https://${host}/",
+      '--registration-token', $registration_token,
+      '--description', $facts['fqdn'],
+      '--tag-list', $tag_list.join(','),
+    ].flatten
 
-  exec { "gitlab-runner-${name}-register":
-    command => shellquote($register_command),
-    creates => "/srv/gitlab-runner/${name}/config.toml",
-    require => File["/srv/gitlab-runner/${name}"],
-  }
+    exec { "gitlab-runner-${name}-register":
+      command => shellquote($register_command),
+      creates => "/srv/gitlab-runner/${name}/config.toml",
+      require => File["/srv/gitlab-runner/${name}"],
+    }
 
-  nest::lib::container { "gitlab-runner-${name}":
-    image   => 'gitlab/gitlab-runner',
-    volumes => [
-      '/run/podman/podman.sock:/var/run/docker.sock',
-      "/srv/gitlab-runner/${name}:/etc/gitlab-runner",
-    ],
-    require => Exec["gitlab-runner-${name}-register"],
+    nest::lib::container { "gitlab-runner-${name}":
+      ensure  => $ensure,
+      image   => 'gitlab/gitlab-runner',
+      volumes => [
+        '/run/podman/podman.sock:/var/run/docker.sock',
+        "/srv/gitlab-runner/${name}:/etc/gitlab-runner",
+      ],
+      require => Exec["gitlab-runner-${name}-register"],
+    }
   }
 }
