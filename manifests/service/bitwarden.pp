77a098329ad5b2fbc43b803720f6c705549100ab
James T. Lee <james@james.tl>
2020-12-17 10:37:50 -0500

    bitwarden: Remove dns parameter
    This was meant to let the container resolve web.nest, but it seems to
    work just fine without.  The dns parameter is absolutely required for
    the linuxserver containers, which reliably fail to look up nest hosts
    without an explicit configuration to use nest DNS.  I think that has
    more to do with the way those containers are made.

--- manifests/service/bitwarden.pp
+++ manifests/service/bitwarden.pp
@@ -14,7 +14,6 @@ class nest::service::bitwarden (
   ->
   nest::lib::container { 'bitwarden':
     image   => 'bitwardenrs/server',
-    dns     => '172.22.0.1',
     env     => [
       'DOMAIN=https://vault.thesatelliteoflove.net',
       "ADMIN_TOKEN=${admin_token}",
