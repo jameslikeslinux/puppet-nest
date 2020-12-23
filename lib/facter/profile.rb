82b21e07b41e4d07fc2f43d9bd2e85ec7569eeb8
James T. Lee <james@james.tl>
2020-12-23 15:39:40 -0500

    profile: Fix role variable

--- lib/facter/profile.rb
+++ lib/facter/profile.rb
@@ -4,7 +4,7 @@ Facter.add('profile') do
     profile = Facter::Core::Execution.execute('/usr/bin/eselect --brief profile show')
     case profile
     when %r{nest:(\S+)/(\S+)/(\S+)}
-      { :cpu => $1, :platform => $2, :role => $2 }
+      { :cpu => $1, :platform => $2, :role => $3 }
     when %r{nest:(\S+)/(\S+)}
       { :cpu => $1, :role => $2 }
     end
