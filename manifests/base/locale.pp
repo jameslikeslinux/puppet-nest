c80a1564a1dfa4acabfa9f26cd418e96dbe6d2bf
James T. Lee <james@james.tl>
2020-12-31 15:28:47 -0500

    locale: Set for both systemd and profile simultaneously

--- manifests/base/locale.pp
+++ manifests/base/locale.pp
@@ -4,15 +4,13 @@ class nest::base::locale {
     line  => 'en_US.UTF-8 UTF-8',
     match => '^#?en_US.UTF-8 UTF-8$',
   }
-
+  ~>
   exec { '/usr/sbin/locale-gen':
     timeout     => 0,
     refreshonly => true,
-    subscribe   => File_line['locale.gen-en_US.UTF-8'],
   }
-
-  file { '/etc/locale.conf':
-    content => "LANG=en_US.UTF-8\n",
-    require => Exec['/usr/sbin/locale-gen'],
+  ->
+  exec { '/usr/bin/eselect locale set en_US.UTF-8':
+    unless => '/usr/bin/eselect --brief locale show | /bin/grep en_US.UTF-8',
   }
 }
