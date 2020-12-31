6459e5c0fa6814628c1f3b3526cdd0a1237dfca5
James T. Lee <james@james.tl>
2020-12-31 14:22:32 -0500

    gitlab-runner: Increase job log limit more

--- manifests/lib/gitlab_runner.pp
+++ manifests/lib/gitlab_runner.pp
@@ -63,7 +63,7 @@ define nest::lib::gitlab_runner (
       '-v', "/srv/gitlab-runner/${name}:/etc/gitlab-runner",
       'gitlab/gitlab-runner', 'register',
       '--non-interactive',
-      '--output-limit', '32768',
+      '--output-limit', '131072',
       '--executor', 'docker',
       '--docker-image', $default_image,
       '--env', "CI_HOST_EMERGE_DEFAULT_OPTS=${::nest::base::portage::emerge_default_opts}",
