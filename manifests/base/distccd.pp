1072c8e0eb1b46e1bba3e9844cd117aa00e6b673
James T. Lee <james@james.tl>
2021-01-09 23:44:42 -0500

    distccd: Manage config everywhere
    It is installed everywhere, so it should be managed everywhere.

--- manifests/base/distccd.pp
+++ manifests/base/distccd.pp
@@ -1,23 +1,21 @@
 class nest::base::distccd {
-  if $::nest::distcc_server {
-    file { '/etc/systemd/system/distccd.service.d/00gentoo.conf':
-      ensure  => absent,
-    }
+  file { '/etc/systemd/system/distccd.service.d/00gentoo.conf':
+    ensure  => absent,
+  }
 
-    file { '/etc/systemd/system/distccd.service.d/10-allowed-servers.conf':
-      mode    => '0644',
-      owner   => 'root',
-      group   => 'root',
-      content => "[Service]\nEnvironment=\"ALLOWED_SERVERS=0.0.0.0/0\"\n",
-      notify  => Nest::Lib::Systemd_reload['distccd'],
-    }
+  file { '/etc/systemd/system/distccd.service.d/10-allowed-servers.conf':
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    content => "[Service]\nEnvironment=\"ALLOWED_SERVERS=0.0.0.0/0\"\n",
+    notify  => Nest::Lib::Systemd_reload['distccd'],
+  }
 
-    ::nest::lib::systemd_reload { 'distccd':
-      notify => Service['distccd']
-    }
+  ::nest::lib::systemd_reload { 'distccd':
+    notify => Service['distccd']
+  }
 
-    service { 'distccd':
-      enable => true,
-    }
+  service { 'distccd':
+    enable => $::nest::distcc_server
   }
 }
