ff724ac34508b7b30a570ee61ec77856a752e531
James T. Lee <james@james.tl>
2021-03-14 17:58:09 -0400

    bootloaders: Only install systemd-boot and grub when /boot is mounted
    This will cause /boot to be unmanaged under stage3, then we can install
    the bootloaders properly during image installation, when the final
    target devices are available.

--- manifests/base/bootloader/grub.pp
+++ manifests/base/bootloader/grub.pp
@@ -1,23 +1,4 @@
 class nest::base::bootloader::grub {
-  # Install stuff normally handled by kernel-install(8)
-  exec {
-    default:
-      refreshonly => true,
-    ;
-
-    'kernel-install':
-      command => '/usr/bin/make install',
-      cwd     => '/usr/src/linux',
-    ;
-
-    'dracut':
-      command  => 'version=$(ls /lib/modules | sort -V | tail -1) && dracut --force --kver $version',
-      provider => shell,
-    ;
-  }
-
-  $font = "ter-x${::nest::console_font_size}b"
-
   nest::lib::package_use { 'sys-boot/grub':
     use => ['grub_platforms_efi-64', 'grub_platforms_pc', 'libzfs', 'truetype'],
   }
@@ -26,8 +7,26 @@ class nest::base::bootloader::grub {
     ensure => installed,
   }
 
-  # Don't manage grub on live-booted systems
-  unless $::mountpoints['/'] and $::mountpoints['/']['device'] == '/dev/mapper/live-rw' {
+  if $facts['mountpoints']['/boot'] {
+    # Install stuff normally handled by kernel-install(8)
+    exec {
+      default:
+        refreshonly => true,
+      ;
+
+      'kernel-install':
+        command => '/usr/bin/make install',
+        cwd     => '/usr/src/linux',
+      ;
+
+      'dracut':
+        command  => 'version=$(ls /lib/modules | sort -V | tail -1) && dracut --force --kver $version',
+        provider => shell,
+      ;
+    }
+
+    $font = "ter-x${::nest::console_font_size}b"
+
     file { '/boot/grub':
       ensure => directory,
       mode   => '0755',
@@ -67,84 +66,78 @@ class nest::base::bootloader::grub {
       source => 'puppet:///modules/nest/keymaps/dvorak.gkb',
     }
 
-    unless $facts['is_container'] {
-      exec { 'grub-mkconfig':
-        command     => '/usr/sbin/grub-mkconfig -o /boot/grub/grub.cfg',
-        refreshonly => true,
-        require     => Exec['grub-mkfont', 'kernel-install', 'dracut'],
-      }
-
-      $file_line_notify = Exec['grub-mkconfig']
+    exec { 'grub-mkconfig':
+      command     => '/usr/sbin/grub-mkconfig -o /boot/grub/grub.cfg',
+      refreshonly => true,
+      require     => Exec['grub-mkfont', 'kernel-install', 'dracut'],
     }
 
-    $grub_install_before = File['/boot/grub']
-  }
+    File_line {
+      path    => '/etc/default/grub',
+      require => Package['sys-boot/grub'],
+      notify  => Exec['grub-mkconfig'],
+    }
 
-  File_line {
-    path    => '/etc/default/grub',
-    require => Package['sys-boot/grub'],
-    notify  => $file_line_notify,
-  }
+    file_line { 'grub-set-kernel-cmdline':
+      line  => "GRUB_CMDLINE_LINUX=\"${::nest::base::bootloader::kernel_cmdline}\"",
+      match => '^#?GRUB_CMDLINE_LINUX=',
+    }
 
-  file_line { 'grub-set-kernel-cmdline':
-    line  => "GRUB_CMDLINE_LINUX=\"${::nest::base::bootloader::kernel_cmdline}\"",
-    match => '^#?GRUB_CMDLINE_LINUX=',
-  }
+    if $facts['virtual'] == 'kvm' {
+      $gfxmode    = 'GRUB_GFXMODE=1024x768'
+      $gfxpayload = 'GRUB_GFXPAYLOAD_LINUX=keep'
+    } else {
+      $gfxmode    = '#GRUB_GFXMODE=640x480'
+      $gfxpayload = '#GRUB_GFXPAYLOAD_LINUX='
+    }
 
-  if $facts['virtual'] == 'kvm' {
-    $gfxmode    = 'GRUB_GFXMODE=1024x768'
-    $gfxpayload = 'GRUB_GFXPAYLOAD_LINUX=keep'
-  } else {
-    $gfxmode    = '#GRUB_GFXMODE=640x480'
-    $gfxpayload = '#GRUB_GFXPAYLOAD_LINUX='
-  }
+    file_line { 'grub-set-gfxmode':
+      line  => $gfxmode,
+      match => '^#?GRUB_GFXMODE',
+    }
 
-  file_line { 'grub-set-gfxmode':
-    line  => $gfxmode,
-    match => '^#?GRUB_GFXMODE',
-  }
+    file_line { 'grub-set-gfxpayload':
+      line  => $gfxpayload,
+      match => '^#?GRUB_GFXPAYLOAD_LINUX',
+    }
 
-  file_line { 'grub-set-gfxpayload':
-    line  => $gfxpayload,
-    match => '^#?GRUB_GFXPAYLOAD_LINUX',
-  }
+    file_line { 'grub-disable-linux-uuid':
+      line  => 'GRUB_DISABLE_LINUX_UUID=true',
+      match => '^#?GRUB_DISABLE_LINUX_UUID=',
+    }
 
-  file_line { 'grub-disable-linux-uuid':
-    line  => 'GRUB_DISABLE_LINUX_UUID=true',
-    match => '^#?GRUB_DISABLE_LINUX_UUID=',
-  }
+    file_line { 'grub-set-device':
+      line  => 'GRUB_DEVICE=zfs:AUTO',
+      match => '^#?GRUB_DEVICE=',
+    }
 
-  file_line { 'grub-set-device':
-    line  => 'GRUB_DEVICE=zfs:AUTO',
-    match => '^#?GRUB_DEVICE=',
-  }
+    file_line { 'grub-set-fs':
+      line  => 'GRUB_FS=',
+      match => '^#?GRUB_FS=',
+    }
 
-  file_line { 'grub-set-fs':
-    line  => 'GRUB_FS=',
-    match => '^#?GRUB_FS=',
-  }
+    file_line { 'grub-set-font':
+      line  => "GRUB_FONT=/boot/grub/fonts/${font}.pf2",
+      match => '^#?GRUB_FONT=',
+    }
 
-  file_line { 'grub-set-font':
-    line  => "GRUB_FONT=/boot/grub/fonts/${font}.pf2",
-    match => '^#?GRUB_FONT=',
-  }
+    $::partitions.each |$partition, $attributes| {
+      $disk = regsubst($partition, 'p?(art)?\d+$', '')
 
-  $::partitions.each |$partition, $attributes| {
-    $disk = regsubst($partition, 'p?(art)?\d+$', '')
+      if "${::trusted['certname']}-" in $attributes['partlabel'] {
+        $grub_install_command = $attributes['partlabel'] ? {
+          /-efi/  => "/bin/mkdir /boot/efi && /bin/mount ${partition} /boot/efi && /usr/sbin/grub-install --target=x86_64-efi --removable --modules=part_gpt && /bin/umount /boot/efi && /bin/rm -rf /boot/efi",
+          /-bios/ => "/usr/sbin/grub-install --target=i386-pc --modules=part_gpt ${disk}",
+          default => undef,
+        }
 
-    if "${::trusted['certname']}-" in $attributes['partlabel'] {
-      $grub_install_command = $attributes['partlabel'] ? {
-        /-efi/  => "/bin/mkdir /boot/efi && /bin/mount ${partition} /boot/efi && /usr/sbin/grub-install --target=x86_64-efi --removable --modules=part_gpt && /bin/umount /boot/efi && /bin/rm -rf /boot/efi",
-        /-bios/ => "/usr/sbin/grub-install --target=i386-pc --modules=part_gpt ${disk}",
-        default => undef,
-      }
-
-      if $grub_install_command {
-        exec { "grub-install-${disk}":
-          command     => $grub_install_command,
-          refreshonly => true,
-          require     => Package['sys-boot/grub'],
-          before      => $grub_install_before,
+        if $grub_install_command {
+          exec { "grub-install-${disk}":
+            command     => $grub_install_command,
+            refreshonly => true,
+            require     => Package['sys-boot/grub'],
+            before      => File['/boot/grub'],
+          }
         }
       }
     }
--- manifests/base/bootloader/systemd.pp
+++ manifests/base/bootloader/systemd.pp
@@ -1,22 +1,5 @@
 class nest::base::bootloader::systemd {
-  case $facts['os']['architecture'] {
-    'amd64': {
-      $boot_efi = 'bootx64.efi'
-      $image    = '/usr/src/linux/arch/x86/boot/bzImage'
-    }
-
-    'aarch64': {
-      $boot_efi = 'bootaa64.efi'
-      $image    = '/usr/src/linux/arch/arm64/boot/Image'
-    }
-
-    'armv7l': {
-      $boot_efi = 'bootarm.efi'
-      $image    = '/usr/src/linux/arch/arm/boot/zImage'
-    }
-  }
-
-  if $mountpoints['/boot'] {
+  if $facts['mountpoints']['/boot'] {
     if $facts['is_container'] or $facts['dmi']['bios']['vendor'] == 'U-Boot' {
       $bootctl_args = '--no-variables'
     } else {
@@ -33,59 +16,34 @@ class nest::base::bootloader::systemd {
       command     => "/usr/bin/bootctl update --no-variables",
       refreshonly => true,
     }
-  } else {
+
     file {
       default:
-        mode  => '0755',
+        mode  => '0644',
         owner => 'root',
         group => 'root',
       ;
 
-      [
-        '/boot/EFI',
-        '/boot/EFI/BOOT',
-      ]:
+      '/etc/kernel':
         ensure => directory,
       ;
 
-      "/boot/EFI/BOOT/${boot_efi.upcase}":
-        source => "/usr/lib/systemd/boot/efi/systemd-${boot_efi}",
-      ;
-
-      "/boot/${facts['machine_id']}":
-        ensure => directory,
-        before => Exec['kernel-install'],
+      '/etc/kernel/cmdline':
+        content => "root=zfs:AUTO ${::nest::base::bootloader::kernel_cmdline}\n",
+        notify  => Exec['kernel-install'],
       ;
     }
 
-    exec { 'kernel-install-fix-boot-path':
-      command     => '/bin/sed -i "s@/boot/@//@g" /boot/loader/entries/*.conf',
+    $image = $facts['os']['architecture'] ? {
+      'amd64'   => '/usr/src/linux/arch/x86/boot/bzImage',
+      'armv7l'  => '/usr/src/linux/arch/arm/boot/zImage',
+      'aarch64' => '/usr/src/linux/arch/arm64/boot/Image',
+    }
+
+    exec { 'kernel-install':
+      command     => "version=\$(ls /lib/modules | sort -V | tail -1) && kernel-install add \$version ${image}",
       provider    => shell,
       refreshonly => true,
-      subscribe   => Exec['kernel-install'],
     }
-  }
-
-  file {
-    default:
-      mode  => '0644',
-      owner => 'root',
-      group => 'root',
-    ;
-
-    '/etc/kernel':
-      ensure => directory,
-    ;
-
-    '/etc/kernel/cmdline':
-      content => "root=zfs:AUTO ${::nest::base::bootloader::kernel_cmdline}\n",
-      notify  => Exec['kernel-install'],
-    ;
-  }
-
-  exec { 'kernel-install':
-    command     => "version=\$(ls /lib/modules | sort -V | tail -1) && kernel-install add \$version ${image}",
-    provider    => shell,
-    refreshonly => true,
   }
 }
