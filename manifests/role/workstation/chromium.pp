f9601d3efe452ac358347b0d4f3cfadf87d7d034
James T. Lee <james@james.tl>
2021-04-22 23:50:35 -0400

    chromium: Defer install until stage2
    Let Chromium be built on its own in a separate project.

--- manifests/role/workstation/chromium.pp
+++ manifests/role/workstation/chromium.pp
@@ -5,39 +5,33 @@ class nest::role::workstation::chromium {
         use => 'widevine',
       }
 
-      package { 'www-client/chromium':
-        ensure => installed,
-      }
-
-      if $facts['architecture'] == 'amd64' {
-        package { 'www-plugins/chrome-binary-plugins':
+      unless $facts['build'] == 'stage1' {
+        package { 'www-client/chromium':
           ensure => installed,
         }
-      }
 
-      $xdg_session_type = '$XDG_SESSION_TYPE'
-      $chromium_flags = @("EOT"/$)
-        [[ $xdg_session_type == 'x11' ]] && CHROMIUM_FLAGS="\${CHROMIUM_FLAGS} --force-device-scale-factor=${::nest::gui_scaling_factor} --enable-use-zoom-for-dsf"
-        CHROMIUM_FLAGS="\${CHROMIUM_FLAGS} --disable-smooth-scrolling"
-        CHROMIUM_FLAGS="\${CHROMIUM_FLAGS} --enable-gpu-rasterization"
-        CHROMIUM_FLAGS="\${CHROMIUM_FLAGS} --enable-oop-rasterization"
-        CHROMIUM_FLAGS="\${CHROMIUM_FLAGS} --ignore-gpu-blocklist"
-        | EOT
+        if $facts['architecture'] == 'amd64' {
+          package { 'www-plugins/chrome-binary-plugins':
+            ensure => installed,
+          }
+        }
 
-      file { '/etc/chromium/nest':
-        mode    => '0644',
-        owner   => 'root',
-        group   => 'root',
-        content => $chromium_flags,
-        require => Package['www-client/chromium'],
-      }
+        $xdg_session_type = '$XDG_SESSION_TYPE'
+        $chromium_flags = @("EOT"/$)
+          [[ $xdg_session_type == 'x11' ]] && CHROMIUM_FLAGS="\${CHROMIUM_FLAGS} --force-device-scale-factor=${::nest::gui_scaling_factor} --enable-use-zoom-for-dsf"
+          CHROMIUM_FLAGS="\${CHROMIUM_FLAGS} --disable-smooth-scrolling"
+          CHROMIUM_FLAGS="\${CHROMIUM_FLAGS} --enable-gpu-rasterization"
+          CHROMIUM_FLAGS="\${CHROMIUM_FLAGS} --enable-oop-rasterization"
+          CHROMIUM_FLAGS="\${CHROMIUM_FLAGS} --ignore-gpu-blocklist"
+          | EOT
 
-      file { [
-        '/etc/chromium/gpu',
-        '/etc/chromium/scaling',
-        '/etc/chromium/scrolling',
-      ]:
-        ensure  => absent,
+        file { '/etc/chromium/nest':
+          mode    => '0644',
+          owner   => 'root',
+          group   => 'root',
+          content => $chromium_flags,
+          require => Package['www-client/chromium'],
+        }
       }
     }
 
