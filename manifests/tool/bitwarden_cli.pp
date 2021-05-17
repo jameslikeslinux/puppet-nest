a7d14a0139fada1449819f970f953c29d89fb839
James T. Lee <james@james.tl>
2021-05-17 12:05:41 -0400

    bitwarden-cli: Install to /opt

--- manifests/tool/bitwarden_cli.pp
+++ manifests/tool/bitwarden_cli.pp
@@ -1,8 +1,19 @@
 class nest::tool::bitwarden_cli {
   include 'nodejs'
 
-  package { '@bitwarden/cli':
-    ensure   => installed,
-    provider => npm,
+  file { '/opt/bitwarden':
+    ensure => directory,
+    mode   => '0755',
+    owner  => 'root',
+    group  => 'root',
+  }
+  ->
+  nodejs::npm { '@bitwarden/cli':
+    target => '/opt/bitwarden',
+  }
+  ->
+  file { '/opt/bitwarden/bw':
+    ensure => link,
+    target => '/opt/bitwarden/node_modules/@bitwarden/cli/build/bw.js',
   }
 }
