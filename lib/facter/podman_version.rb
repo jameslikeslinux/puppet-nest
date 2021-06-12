cb0bf4ca4d5b113c037c2f4998789fc32495fb36
James T. Lee <james@james.tl>
2021-06-12 03:12:40 -0400

    Fix podman_version fact

--- lib/facter/podman_version.rb
+++ lib/facter/podman_version.rb
@@ -2,7 +2,7 @@ Facter.add('podman_version') do
   confine kernel: 'Linux'
   setcode do
     if File.exist?('/usr/bin/podman')
-      Regexp.last_match(1) if Facter::Core::Execution.execute('/usr/bin/podman --version') =~ %{podman version (\S+)}
+      Regexp.last_match(1) if Facter::Core::Execution.execute('/usr/bin/podman --version') =~ %r{podman version (\S+)}
     end
   end
 end
