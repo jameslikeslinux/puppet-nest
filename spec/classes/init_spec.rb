232a8c23b882ca68031cc32bc79ab299b324ce99
James T. Lee <james@james.tl>
2021-06-12 02:19:33 -0400

    Remove old module skeleton artifacts

--- spec/classes/init_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-describe 'nest' do
-  on_supported_os(facterversion: '2.4').each do |os, facts|
-    context "on #{os}" do
-      let(:facts) { facts }
-      it { should compile.with_all_deps }
-    end
-  end
-end
