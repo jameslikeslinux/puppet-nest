6f44403397af85f068b60e0a39c8b9d1d33165dd
James T. Lee <james@james.tl>
2021-07-07 17:16:58 -0400

    Revert "cursor: Fix size under Plasma with HiDPI"
    This reverts commit 41238ca145593321ca6c3590b75a16e5408e8ff4.

--- manifests/role/workstation/cursor.pp
+++ manifests/role/workstation/cursor.pp
@@ -24,20 +24,11 @@ class nest::role::workstation::cursor {
       group => 'root';
 
     '/etc/X11/Xresources':
-      content => "Xcursor.size: ${::nest::cursor_size}\n",
-    ;
+      content => "Xcursor.size: ${::nest::cursor_size}\n";
 
-    [
-      '/etc/plasma',
-      '/etc/plasma/startup',
-    ]:
-      ensure => directory,
-    ;
-
-
+    # XCURSOR_SIZE management is pretty good now
     '/etc/plasma/startup/10-cursor.sh':
-      content => "export XCURSOR_SIZE=${::nest::cursor_size}\n",
-    ;
+      ensure => absent,
   }
 
   file_line { 'sddm-load-xresources':
