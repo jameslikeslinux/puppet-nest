a164af983085b71f3a4e5ceea597bd65de005373
James Lee <james@james.tl>
2022-01-03 18:07:00 -0500

    facts: Let is_container detect systemd-nspawn

--- lib/facter/is_container.rb
+++ lib/facter/is_container.rb
@@ -1,6 +1,6 @@
 Facter.add('is_container') do
   confine kernel: 'Linux'
   setcode do
-    Facter.value(:virtual) == 'lxc' or File.exist? '/run/.containerenv'
+    Facter.value(:virtual) == 'lxc' || File.exist?('/run/.containerenv') || File.exist?('/run/host/container-manager')
   end
 end
