bd3cc680d21c48b625232dabfd2fb8dca874337b
James Lee <james@james.tl>
2022-01-19 18:03:48 -0500

    bolt: Configure connection to PuppetDB

--- manifests/tool/bolt.pp
+++ manifests/tool/bolt.pp
@@ -5,11 +5,33 @@ class nest::tool::bolt {
       provider        => gem,
     }
   } elsif $facts['os']['family'] == 'Gentoo' {
-    file { '/usr/local/bin/bolt':
-      mode    => '0755',
-      owner   => 'root',
-      group   => 'root',
-      content => epp('nest/scripts/bolt.sh.epp'),
+    $ssldir = '/etc/puppetlabs/puppet/ssl'
+
+    file {
+      default:
+        mode  => '0644',
+        owner => 'root',
+        group => 'root',
+      ;
+
+      '/etc/puppetlabs/bolt':
+        ensure => directory,
+      ;
+
+      '/etc/puppetlabs/bolt/bolt-defaults.yaml':
+        content   => epp('nest/puppet/bolt-defaults.yaml.epp', {
+          puppetdb_server_url => 'https://puppetdb.nest:8081',
+          puppetdb_cacert     => "${ssldir}/certs/ca.pem",
+          puppetdb_cert       => "${ssldir}/certs/${trusted['certname']}.pem",
+          puppetdb_key        => "${ssldir}/private_keys/${trusted['certname']}.pem",
+        }),
+        show_diff => false,
+      ;
+
+      '/usr/local/bin/bolt':
+        mode    => '0755',
+        content => epp('nest/scripts/bolt.sh.epp'),
+      ;
     }
   }
 }
