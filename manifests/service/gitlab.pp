880468b828d9b736468128edcedb20e4c09e1297
James Lee <james@james.tl>
2022-02-14 11:24:40 -0500

    gitlab: Support running a local-only instance

--- manifests/service/gitlab.pp
+++ manifests/service/gitlab.pp
@@ -1,7 +1,51 @@
 class nest::service::gitlab (
-  String  $gmail_password,
-  Integer $puma_workers = $::nest::concurrency,
+  String            $external_name,
+  Optional[String]  $registry_external_name = undef,
+  Boolean           $https                  = false,
+  Stdlib::Port      $ssh_port               = 22,
+  Stdlib::Port      $http_port              = 80,
+  Stdlib::Port      $registry_port          = 5050,
+  Optional[Integer] $default_theme          = undef,
+  Optional[String]  $gmail_password         = undef,
+  Integer           $puma_workers           = $::nest::concurrency,
 ) inherits nest {
+  if $https {
+    $external_url = "https://${external_name}"
+    if $registry_external_name {
+      $registry_url = "https://${registry_external_name}"
+    }
+  } else {
+    if $http_port == '80' {
+      $external_url = "http://${external_name}"
+    } else {
+      $external_url = "http://${external_name}:${http_port}"
+    }
+    if $registry_external_name {
+      if $registry_port == '5050' {
+        $registry_url = "http://${registry_external_name}"
+      } else {
+        $registry_url = "http://${registry_external_name}:${registry_port}"
+      }
+    }
+  }
+
+  $publish = [
+    $ssh_port ? {
+      22      => '2222:22',
+      default => "${ssh_port}:22",
+    },
+
+    $http_port ? {
+      80      => '8000:80',
+      default => "${http_port}:${http_port}",
+    },
+
+    $registry_external_url ? {
+      undef   => [],
+      default => "${registry_port}:${registry_port}",
+    },
+  ].flatten
+
   nest::lib::srv { 'gitlab': }
   ->
   file {
@@ -29,7 +73,7 @@ class nest::service::gitlab (
   nest::lib::container { 'gitlab':
     image   => 'gitlab/gitlab-ce',
     env     => ["GITLAB_OMNIBUS_CONFIG=from_file('/omnibus_config.rb')"],
-    publish => ['2222:22', '8000:80', '5050:5050'],
+    publish => $publish,
     volumes => [
       '/srv/gitlab/gitlab.rb:/omnibus_config.rb:ro',
       '/srv/gitlab/config:/etc/gitlab',
@@ -39,9 +83,9 @@ class nest::service::gitlab (
   }
 
   # Export SSH keys for collecting on other hosts
-  if $facts['gitlab_ssh'] {
+  if $facts['gitlab_ssh'] and $external_name != 'localhost' {
     $facts['gitlab_ssh'].each |$key, $value| {
-      @@sshkey { "gitlab.james.tl@${value['type']}":
+      @@sshkey { "${external_name}@${value['type']}":
         key => $value['key'],
       }
     }
