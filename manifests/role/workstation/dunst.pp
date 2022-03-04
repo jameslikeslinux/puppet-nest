5d4f83bc2ca290beae5d783343c2620aa72178f5
James Lee <james@james.tl>
2022-03-04 14:03:10 -0500

    dunst: Remove scaled icons in favor of native scaling

--- manifests/role/workstation/dunst.pp
+++ manifests/role/workstation/dunst.pp
@@ -7,52 +7,12 @@ class nest::role::workstation::dunst {
     ensure => installed,
   }
 
-  File {
-    mode      => '0644',
-    owner     => 'root',
-    group     => 'root',
-    purge     => true,
-    recurse   => true,
-    force     => true,
-    max_files => 10000,
-  }
 
-  file { [
-    '/usr/share/dunst',
-    '/usr/share/dunst/icons',
-  ]:
-    ensure => directory,
-  }
-
-  $icon_size   = 32
-  $convert_cmd = @("SCRIPT")
-    rsvg-convert -w ${icon_size} -h ${icon_size} -f svg '%' |
-    sed 's/${icon_size}pt/${icon_size}px/g' > '%.tmp' &&
-    mv '%.tmp' '%'
-    | SCRIPT
-
-  ['actions', 'devices', 'status'].each |$category| {
-    file {
-      "/usr/share/dunst/icons/${category}":
-        ensure => directory,
-      ;
-
-      "/usr/share/dunst/icons/${category}/${icon_size}":
-        ensure       => directory,
-        source       => "/usr/share/icons/breeze-dark/${category}/22",
-        backup       => false,
-        show_diff    => false,
-        validate_cmd => $convert_cmd,
-        checksum     => mtime,
-        links        => follow,
-        require      => Package['gnome-base/librsvg'],
-      ;
-
-      "/usr/share/dunst/icons/${category}/scaled":
-        ensure  => link,
-        target  => "/usr/share/dunst/icons/${category}/${icon_size}",
-        recurse => false,
-      ;
-    }
+  #
+  # XXX: Cleanup scaled icons in favor of native scaling
+  #
+  file { '/usr/share/dunst':
+    ensure => absent,
+    force  => true,
   }
 }
