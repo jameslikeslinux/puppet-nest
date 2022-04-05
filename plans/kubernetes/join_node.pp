e463ca1746793f24abd18001e0aa7abcb8d6a7d2
James Lee <james@james.tl>
2022-04-05 14:39:39 -0400

    k8s: Simplify plan for getting join command

--- plans/kubernetes/join_node.pp
+++ plans/kubernetes/join_node.pp
@@ -8,21 +8,12 @@ plan nest::kubernetes::join_node (
   TargetSpec $targets,
   TargetSpec $control_plane,
 ) {
-  $kubeadm_token = run_command('kubeadm token create', get_targets($control_plane)[0], 'Get kubeadm token', {
+  $kubeadm_token_cmd = 'kubeadm token create --print-join-command'
+
+  $kubeadm_join_cmd = run_command($kubeadm_token_cmd, get_targets($control_plane)[0], 'Get kubeadm join command', {
     _run_as => 'root',
   }).first.value['stdout'].chomp
 
-  $ca_cert_hash_cmd = @(HASH_CMD)
-    openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt |
-    openssl rsa -pubin -outform der 2>/dev/null |
-    openssl dgst -sha256 -hex |
-    sed 's/^.* //'
-    | HASH_CMD
-  $ca_cert_hash = run_command($ca_cert_hash_cmd, get_targets($control_plane)[0], 'Get discovery token CA cert hash', {
-    _run_as => 'root',
-  }).first.value['stdout'].chomp
-
-  $kubeadm_join_cmd = "kubeadm join ${control_plane}:6443 --token ${kubeadm_token} --discovery-token-ca-cert-hash sha256:${ca_cert_hash}"
   run_command($kubeadm_join_cmd, $targets, 'Join node to Kubernetes cluster', {
     _run_as => 'root',
   })
