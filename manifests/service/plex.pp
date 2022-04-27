05f854de5b23de5732b081e5387b624fb17b1b49
James Lee <james@james.tl>
2022-04-27 16:19:01 -0400

    plex: Switch back to official container image

--- manifests/service/plex.pp
+++ manifests/service/plex.pp
@@ -1,14 +1,13 @@
 class nest::service::plex {
   nest::lib::container { 'plex':
-    ensure  => absent,
     image   => 'plexinc/pms-docker',
-    env     => ['PLEX_UID=32400', 'PLEX_GID=1001', 'TZ=America/New_York'],
+    env     => ['PLEX_UID=1001', 'PLEX_GID=1001', 'TZ=America/New_York'],
     network => 'host',
     tmpfs   => ['/transcode'],
     volumes => [
       '/srv/plex/config:/config',
-      '/nest/movies:/movies',
-      '/nest/tv:/tv',
+      '/nest/movies:/movies:ro',
+      '/nest/tv:/tv:ro',
     ],
   }
 
