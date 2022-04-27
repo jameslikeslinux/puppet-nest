da8d022fed8528decf6bb65240e0ef59e61fcbed
James Lee <james@james.tl>
2022-04-27 15:42:45 -0400

    Revert "plex: Migrate to system package"
    I should have figured that the system package also uses musl making it
    incompatible with the system VA-API drivers.
    
    This reverts commit 7d268345f1dc0ffab780378a605df07bebcf2158.

--- manifests/service/plex.pp
+++ manifests/service/plex.pp
@@ -1,10 +1,15 @@
 class nest::service::plex {
-  nest::lib::package { 'media-tv/plex-media-server':
-    ensure => installed,
-  }
-  ->
-  service { 'plexmediaserver':
-    enable => true,
+  nest::lib::container { 'plex':
+    ensure  => absent,
+    image   => 'plexinc/pms-docker',
+    env     => ['PLEX_UID=32400', 'PLEX_GID=1001', 'TZ=America/New_York'],
+    network => 'host',
+    tmpfs   => ['/transcode'],
+    volumes => [
+      '/srv/plex/config:/config',
+      '/nest/movies:/movies',
+      '/nest/tv:/tv',
+    ],
   }
 
   firewalld_service { 'plex':
