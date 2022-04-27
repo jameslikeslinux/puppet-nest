7d268345f1dc0ffab780378a605df07bebcf2158
James Lee <james@james.tl>
2022-04-27 13:05:11 -0400

    plex: Migrate to system package
    The official Plex container image uses musl libc making it very
    difficult to map in VA-API support into the container. Plex doesn't
    officially support AMD video acceleration, but when it's available via
    VA-API, it is able to use it.

--- manifests/service/plex.pp
+++ manifests/service/plex.pp
@@ -1,15 +1,10 @@
 class nest::service::plex {
-  nest::lib::container { 'plex':
-    ensure  => absent,
-    image   => 'plexinc/pms-docker',
-    env     => ['PLEX_UID=32400', 'PLEX_GID=1001', 'TZ=America/New_York'],
-    network => 'host',
-    tmpfs   => ['/transcode'],
-    volumes => [
-      '/srv/plex/config:/config',
-      '/nest/movies:/movies',
-      '/nest/tv:/tv',
-    ],
+  nest::lib::package { 'media-tv/plex-media-server':
+    ensure => installed,
+  }
+  ->
+  service { 'plexmediaserver':
+    enable => true,
   }
 
   firewalld_service { 'plex':
