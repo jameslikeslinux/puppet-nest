3cab026e0ac93d5e5f6c51552ca48bcd006c0cde
James Lee <james@james.tl>
2022-05-31 16:06:53 -0400

    containers: Disable all container mgmt inside containers
    Let updates work inside of systemd-nspawn containers. Podman just
    doesn't work as expected inside of containers. Not a surprise.

--- manifests/lib/container.pp
+++ manifests/lib/container.pp
@@ -9,156 +9,153 @@ define nest::lib::container (
   Array[String]       $tmpfs   = [],
   Array[String]       $volumes = [],
 ) {
-  # Required for /usr/bin/podman
-  include 'nest'
+  unless $facts['is_container'] {
+    # Required for /usr/bin/podman
+    include 'nest'
 
-  if $ensure == absent {
-    service { "container-${name}":
-      ensure => stopped,
-      enable => false,
-    }
-    ->
-    exec { "remove-container-${name}":
-      command => "/usr/bin/podman container rm ${name.shellquote}",
-      onlyif  => "/usr/bin/podman container exists ${name.shellquote}",
-      before  => Nest::Lib::Pod[$pod],
-    }
+    if $ensure == absent {
+      service { "container-${name}":
+        ensure => stopped,
+        enable => false,
+      }
+      ->
+      exec { "remove-container-${name}":
+        command => "/usr/bin/podman container rm ${name.shellquote}",
+        onlyif  => "/usr/bin/podman container exists ${name.shellquote}",
+        before  => Nest::Lib::Pod[$pod],
+      }
 
-    file { "/etc/systemd/system/container-${name}.service":
-      ensure  => absent,
-    }
-    ~>
-    nest::lib::systemd_reload { "container-${name}":
-      require => Service["container-${name}"],
-    }
+      file { "/etc/systemd/system/container-${name}.service":
+        ensure  => absent,
+      }
+      ~>
+      nest::lib::systemd_reload { "container-${name}":
+        require => Service["container-${name}"],
+      }
 
-    if $pod {
-      exec { "regenerate-services-pod-${pod}-${name}":
-        command     => "/usr/bin/podman generate systemd --files --name ${pod.shellquote}",
+      if $pod {
+        exec { "regenerate-services-pod-${pod}-${name}":
+          command     => "/usr/bin/podman generate systemd --files --name ${pod.shellquote}",
+          cwd         => '/etc/systemd/system',
+          refreshonly => true,
+          subscribe   => Exec["remove-container-${name}"],
+          notify      => Nest::Lib::Systemd_reload["container-${name}"],
+        }
+      }
+    } else {
+      if $pod and !empty($publish) {
+        fail("Ports must be published on the pod '${pod}'")
+      }
+
+      case $ensure {
+        running: {
+          $service_ensure = running
+          $service_enable = true
+        }
+
+        enabled: {
+          $service_ensure = undef
+          $service_enable = true
+        }
+
+        present: {
+          $service_ensure = undef
+          $service_enable = undef
+        }
+
+        disabled: {
+          $service_ensure = undef
+          $service_enable = false
+        }
+
+        stopped: {
+          $service_ensure = stopped
+          $service_enable = false
+        }
+
+        default: {
+          fail("Unhandled value for ensure: '${ensure}'")
+        }
+      }
+
+      $dns_args = $dns ? {
+        undef   => [],
+        default => ["--dns=${dns}"],
+      }
+
+      $env_args = $env.map |$e| {
+        "--env=${e}"
+      }
+
+      $network_args = $network ? {
+        undef   => [],
+        default => ["--network=${network}"],
+      }
+
+      $pod_args = $pod ? {
+        undef   => [],
+        default => ["--pod=${pod}"],
+      }
+
+      $publish_args = $publish.map |$e| {
+        "--publish=${e}"
+      }
+
+      $tmpfs_args = $tmpfs.map |$t| {
+        "--tmpfs=${t}"
+      }
+
+      $volumes_args = $volumes.map |$volume| {
+        "--volume=${volume}"
+      }
+
+      $podman_create_cmd = [
+        '/usr/bin/podman', 'container', 'create',
+        '--replace',
+        $dns_args,
+        $env_args,
+        $network_args,
+        $pod_args,
+        $publish_args,
+        $tmpfs_args,
+        $volumes_args,
+        "--label=nest.podman.version=${facts['podman_version']}",
+        "--name=${name}",
+        $image,
+      ].flatten
+
+      $podman_create_str = "[${podman_create_cmd.join(' ')}]"
+      $podman_inspect_create_command = [
+        '/usr/bin/podman', 'container', 'inspect',
+        '--format={{.Config.CreateCommand}}',
+        $name,
+      ]
+
+      exec { "stop-container-${name}":
+        command => "/bin/systemctl stop container-${name}",
+        returns => [0, 5],
+        unless  => "/usr/bin/test ${podman_create_str.shellquote} = \"`${podman_inspect_create_command.shellquote}`\"",
+        require => Class['nest::base::containers'],
+      }
+      ~>
+      exec { "create-container-${name}":
+        command     => shellquote($podman_create_cmd),
+        refreshonly => true,
+        require     => Nest::Lib::Pod[$pod],
+      }
+      ~>
+      exec { "generate-services-container-${name}":
+        command     => "/usr/bin/podman generate systemd --files --name ${pick($pod, $name).shellquote}",
         cwd         => '/etc/systemd/system',
         refreshonly => true,
-        subscribe   => Exec["remove-container-${name}"],
-        notify      => Nest::Lib::Systemd_reload["container-${name}"],
       }
-    }
-  } else {
-    if $pod and !empty($publish) {
-      fail("Ports must be published on the pod '${pod}'")
-    }
-
-    case $ensure {
-      running: {
-        $service_ensure = running
-        $service_enable = true
+      ~>
+      nest::lib::systemd_reload { "container-${name}": }
+      ->
+      service { "container-${name}":
+        ensure => $service_ensure,
+        enable => $service_enable,
       }
-
-      enabled: {
-        $service_ensure = undef
-        $service_enable = true
-      }
-
-      present: {
-        $service_ensure = undef
-        $service_enable = undef
-      }
-
-      disabled: {
-        $service_ensure = undef
-        $service_enable = false
-      }
-
-      stopped: {
-        $service_ensure = stopped
-        $service_enable = false
-      }
-
-      default: {
-        fail("Unhandled value for ensure: '${ensure}'")
-      }
-    }
-
-    $dns_args = $dns ? {
-      undef   => [],
-      default => ["--dns=${dns}"],
-    }
-
-    $env_args = $env.map |$e| {
-      "--env=${e}"
-    }
-
-    $network_args = $network ? {
-      undef   => [],
-      default => ["--network=${network}"],
-    }
-
-    $pod_args = $pod ? {
-      undef   => [],
-      default => ["--pod=${pod}"],
-    }
-
-    $publish_args = $publish.map |$e| {
-      "--publish=${e}"
-    }
-
-    $tmpfs_args = $tmpfs.map |$t| {
-      "--tmpfs=${t}"
-    }
-
-    $volumes_args = $volumes.map |$volume| {
-      "--volume=${volume}"
-    }
-
-    $podman_create_cmd = [
-      '/usr/bin/podman', 'container', 'create',
-      '--replace',
-      $dns_args,
-      $env_args,
-      $network_args,
-      $pod_args,
-      $publish_args,
-      $tmpfs_args,
-      $volumes_args,
-      "--label=nest.podman.version=${facts['podman_version']}",
-      "--name=${name}",
-      $image,
-    ].flatten
-
-    $podman_create_str = "[${podman_create_cmd.join(' ')}]"
-    $podman_inspect_create_command = [
-      '/usr/bin/podman', 'container', 'inspect',
-      '--format={{.Config.CreateCommand}}',
-      $name,
-    ]
-
-    $stop_container_cmd = $facts['is_container'] ? {
-      true    => '/bin/true',
-      default => "/bin/systemctl stop container-${name}",
-    }
-
-    exec { "stop-container-${name}":
-      command => $stop_container_cmd,
-      returns => [0, 5],
-      unless  => "/usr/bin/test ${podman_create_str.shellquote} = \"`${podman_inspect_create_command.shellquote}`\"",
-      require => Class['nest::base::containers'],
-    }
-    ~>
-    exec { "create-container-${name}":
-      command     => shellquote($podman_create_cmd),
-      refreshonly => true,
-      require     => Nest::Lib::Pod[$pod],
-    }
-    ~>
-    exec { "generate-services-container-${name}":
-      command     => "/usr/bin/podman generate systemd --files --name ${pick($pod, $name).shellquote}",
-      cwd         => '/etc/systemd/system',
-      refreshonly => true,
-    }
-    ~>
-    nest::lib::systemd_reload { "container-${name}": }
-    ->
-    service { "container-${name}":
-      ensure => $service_ensure,
-      enable => $service_enable,
     }
   }
 }
--- manifests/lib/pod.pp
+++ manifests/lib/pod.pp
@@ -3,107 +3,109 @@ define nest::lib::pod (
   Nest::ServiceEnsure $ensure  = running,
   Array[String]       $publish = [],
 ) {
-  # Required for /usr/bin/podman
-  include 'nest'
+  unless $facts['is_container'] {
+    # Required for /usr/bin/podman
+    include 'nest'
 
-  if $ensure == absent {
-    service { "pod-${name}":
-      ensure => stopped,
-      enable => false,
-    }
-    ->
-    exec { "remove-pod-${name}":
-      command => "/usr/bin/podman pod rm ${name.shellquote}",
-      onlyif  => "/usr/bin/podman pod exists ${name.shellquote}",
-    }
-
-    file { "/etc/systemd/system/pod-${name}.service":
-      ensure => absent,
-    }
-    ~>
-    nest::lib::systemd_reload { "pod-${name}":
-      require => Service["pod-${name}"],
-    }
-  } else {
-    case $ensure {
-      running: {
-        $service_ensure = running
-        $service_enable = true
+    if $ensure == absent {
+      service { "pod-${name}":
+        ensure => stopped,
+        enable => false,
+      }
+      ->
+      exec { "remove-pod-${name}":
+        command => "/usr/bin/podman pod rm ${name.shellquote}",
+        onlyif  => "/usr/bin/podman pod exists ${name.shellquote}",
       }
 
-      enabled: {
-        $service_ensure = undef
-        $service_enable = true
+      file { "/etc/systemd/system/pod-${name}.service":
+        ensure => absent,
+      }
+      ~>
+      nest::lib::systemd_reload { "pod-${name}":
+        require => Service["pod-${name}"],
+      }
+    } else {
+      case $ensure {
+        running: {
+          $service_ensure = running
+          $service_enable = true
+        }
+
+        enabled: {
+          $service_ensure = undef
+          $service_enable = true
+        }
+
+        present: {
+          $service_ensure = undef
+          $service_enable = undef
+        }
+
+        disabled: {
+          $service_ensure = undef
+          $service_enable = false
+        }
+
+        stopped: {
+          $service_ensure = stopped
+          $service_enable = false
+        }
+
+        default: {
+          fail("Unhandled value for ensure: '${ensure}'")
+        }
       }
 
-      present: {
-        $service_ensure = undef
-        $service_enable = undef
+      $dns_args = $dns ? {
+        undef   => [],
+        default => ["--dns=${dns}"],
       }
 
-      disabled: {
-        $service_ensure = undef
-        $service_enable = false
+      $publish_args = $publish.map |$e| {
+        "--publish=${e}"
       }
 
-      stopped: {
-        $service_ensure = stopped
-        $service_enable = false
+      $podman_create_cmd = [
+        '/usr/bin/podman', 'pod', 'create',
+        '--replace',
+        $dns_args,
+        $publish_args,
+        "--label=nest.podman.version=${facts['podman_version']}",
+        "--name=${name}",
+      ].flatten
+
+      $podman_create_str = "[${podman_create_cmd.join(' ')}]"
+      $podman_inspect_create_command = [
+        '/usr/bin/podman', 'pod', 'inspect',
+        '--format={{.CreateCommand}}',
+        $name,
+      ]
+
+      exec { "stop-pod-${name}":
+        command => "/bin/systemctl stop pod-${name}",
+        returns => [0, 5],
+        unless  => "/usr/bin/test ${podman_create_str.shellquote} = \"`${podman_inspect_create_command.shellquote}`\"",
+        require => Class['nest::base::containers'],
       }
-
-      default: {
-        fail("Unhandled value for ensure: '${ensure}'")
+      ~>
+      exec { "create-pod-${name}":
+        command     => shellquote($podman_create_cmd),
+        refreshonly => true,
       }
-    }
-
-    $dns_args = $dns ? {
-      undef   => [],
-      default => ["--dns=${dns}"],
-    }
-
-    $publish_args = $publish.map |$e| {
-      "--publish=${e}"
-    }
-
-    $podman_create_cmd = [
-      '/usr/bin/podman', 'pod', 'create',
-      '--replace',
-      $dns_args,
-      $publish_args,
-      "--label=nest.podman.version=${facts['podman_version']}",
-      "--name=${name}",
-    ].flatten
-
-    $podman_create_str = "[${podman_create_cmd.join(' ')}]"
-    $podman_inspect_create_command = [
-      '/usr/bin/podman', 'pod', 'inspect',
-      '--format={{.CreateCommand}}',
-      $name,
-    ]
-
-    exec { "stop-pod-${name}":
-      command => "/bin/systemctl stop pod-${name}",
-      returns => [0, 5],
-      unless  => "/usr/bin/test ${podman_create_str.shellquote} = \"`${podman_inspect_create_command.shellquote}`\"",
-      require => Class['nest::base::containers'],
-    }
-    ~>
-    exec { "create-pod-${name}":
-      command     => shellquote($podman_create_cmd),
-      refreshonly => true,
-    }
-    ~>
-    exec { "generate-services-pod-${name}":
-      command     => "/usr/bin/podman generate systemd --files --name ${name.shellquote}",
-      cwd         => '/etc/systemd/system',
-      refreshonly => true,
-    }
-    ~>
-    nest::lib::systemd_reload { "pod-${name}": }
-    ->
-    service { "pod-${name}":
-      ensure => $service_ensure,
-      enable => $service_enable,
+      ~>
+      exec { "generate-services-pod-${name}":
+        command     => "/usr/bin/podman generate systemd --files --name ${name.shellquote}",
+        cwd         => '/etc/systemd/system',
+        refreshonly => true,
+      }
+      ~>
+      nest::lib::systemd_reload { "pod-${name}": }
+      ->
+      service { "pod-${name}":
+        ensure => $service_ensure,
+        enable => $service_enable,
+      }
     }
   }
 }
