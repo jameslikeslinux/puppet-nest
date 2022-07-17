31f2ba3fa55059ef604eb8c35fed6108b4c6db6b
James Lee <james@james.tl>
2022-07-17 19:25:31 -0400

    dracut: Use dropin to override systemd-udev-settle
    This is more resilient on upgrades.

--- manifests/base/dracut.pp
+++ manifests/base/dracut.pp
@@ -10,7 +10,7 @@ class nest::base::dracut {
   }
 
   if $facts['profile']['platform'] == 'live' {
-    $base_config_content = @("EOT")
+    $base_config_content = @(EOT)
       add_dracutmodules+=" dmsquash-live "
       omit_dracutmodules+=" zfs "
       kernel_cmdline="rd.live.overlay.overlayfs=1"
@@ -18,7 +18,7 @@ class nest::base::dracut {
   } elsif $facts['build'] and $facts['build'] != 'kernel' {
     $base_config_content = ''
   } else {
-    $base_config_content = @("EOT")
+    $base_config_content = @(EOT)
       force="yes"
       hostonly="yes"
       | EOT
@@ -34,15 +34,51 @@ class nest::base::dracut {
 
   # Add delay to ensure all devices are enumerated at boot before the ZFS import
   # See: https://github.com/openzfs/zfs/issues/8885#issuecomment-774503341
-  file_line { 'systemd-udev-settle-sleep':
-    path => '/lib/systemd/system/systemd-udev-settle.service',
-    line => 'ExecStartPre=/bin/sleep 5',
+  $systemd_udev_settle_sleep = @(END_DROPIN)
+    [Service]
+    ExecStartPre=/bin/sleep 5
+    | END_DROPIN
+  $install_config = @(END_CONF)
+    install_items+=" /etc/systemd/system/systemd-udev-settle.service.d/10-sleep.conf "
+    | END_CONF
+
+  file {
+    default:
+      mode    => '0644',
+      owner   => 'root',
+      group   => 'root',
+    ;
+
+    '/etc/systemd/system/systemd-udev-settle.service.d':
+      ensure => directory,
+    ;
+
+    '/etc/systemd/system/systemd-udev-settle.service.d/10-sleep.conf':
+      content => $systemd_udev_settle_sleep,
+      notify  => Nest::Lib::Systemd_reload['dracut'],
+    ;
+
+    '/etc/dracut.conf.d/10-systemd.conf':
+      content => $install_config,
+    ;
   }
 
+  nest::lib::systemd_reload { 'dracut': }
+
   # XXX: Cleanup
-  file_line { 'systemd-udev-trigger-changes':
-    ensure => absent,
-    path   => '/lib/systemd/system/systemd-udev-trigger.service',
-    line   => 'ExecStart=/bin/udevadm trigger --type=devices --action=change',
+  file_line {
+    default:
+      ensure => absent,
+    ;
+
+    'systemd-udev-settle-sleep':
+      path   => '/lib/systemd/system/systemd-udev-settle.service',
+      line   => 'ExecStartPre=/bin/sleep 5',
+    ;
+
+    'systemd-udev-trigger-changes':
+      path   => '/lib/systemd/system/systemd-udev-trigger.service',
+      line   => 'ExecStart=/bin/udevadm trigger --type=devices --action=change',
+    ;
   }
 }
