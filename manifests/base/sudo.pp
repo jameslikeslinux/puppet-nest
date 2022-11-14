fd51424e2b5d0bc3b93c798979ffe960dcd8a464
James Lee <james@james.tl>
2022-11-13 20:51:20 -0500

    sudo: Make /etc/sudoers.d
    The directory is no longer provided by the package in a recent update.

--- manifests/base/sudo.pp
+++ manifests/base/sudo.pp
@@ -2,20 +2,25 @@ class nest::base::sudo {
   package { 'app-admin/sudo':
     ensure => installed,
   }
+  ->
+  file {
+    default:
+      mode  => '0644',
+      owner => 'root',
+      group => 'root',
+    ;
 
-  file { '/etc/sudoers.d/10_env':
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
-    content => "Defaults env_keep += \"SSH_AUTH_SOCK SSH_CLIENT SSH_CONNECTION TMUX TMUX_PANE XAUTHORITY\"\n",
-    require => Package['app-admin/sudo'],
-  }
+    '/etc/sudoers.d':
+      ensure => directory,
+      mode   => '0750',
+    ;
 
-  file { '/etc/sudoers.d/10_wheel':
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
-    content => "%wheel ALL=(ALL) NOPASSWD: ALL\n",
-    require => Package['app-admin/sudo'],
+    '/etc/sudoers.d/10_env':
+      content => "Defaults env_keep += \"SSH_AUTH_SOCK SSH_CLIENT SSH_CONNECTION TMUX TMUX_PANE XAUTHORITY\"\n",
+    ;
+
+    '/etc/sudoers.d/10_wheel':
+      content => "%wheel ALL=(ALL) NOPASSWD: ALL\n",
+    ;
   }
 }
