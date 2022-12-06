ca22847b516c5a580752c86bc0df67a0f05ce69d
James Lee <james@james.tl>
2022-12-06 18:33:15 -0500

    gitlab-runner: Do not manage inside (build) container

--- manifests/service/gitlab_runner.pp
+++ manifests/service/gitlab_runner.pp
@@ -2,13 +2,15 @@ class nest::service::gitlab_runner (
   String             $registration_token,
   Hash[String, Hash] $instances = {},
 ) {
-  nest::lib::srv { 'gitlab-runner': }
+  unless $facts['is_container'] {
+    nest::lib::srv { 'gitlab-runner': }
 
-  $instances.each |$instance, $attributes| {
-    nest::lib::gitlab_runner { $instance:
-      registration_token => $registration_token,
-      require            => Nest::Lib::Srv['gitlab-runner'],
-      *                  => $attributes,
+    $instances.each |$instance, $attributes| {
+      nest::lib::gitlab_runner { $instance:
+        registration_token => $registration_token,
+        require            => Nest::Lib::Srv['gitlab-runner'],
+        *                  => $attributes,
+      }
     }
   }
 }
