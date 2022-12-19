bffc3860d0f258c78f4dc685580c0b758bce8109
James Lee <james@james.tl>
2022-12-19 18:11:02 -0500

    fail2ban: Refactor and manage jail.d directory

--- manifests/base/fail2ban.pp
+++ manifests/base/fail2ban.pp
@@ -1,42 +1,47 @@
 class nest::base::fail2ban {
   if $nest::public_ssh {
-    package { 'net-analyzer/fail2ban':
-      ensure => installed,
-    }
-
     $sshd_jail_conf = @(SSHD_JAIL)
       [sshd]
       enabled = true
       backend = systemd
       | SSHD_JAIL
 
-    file { '/etc/fail2ban/jail.d/sshd.conf':
-      mode    => '0644',
-      owner   => 'root',
-      group   => 'root',
-      content => $sshd_jail_conf,
-      require => Package['net-analyzer/fail2ban'],
-      notify  => Service['fail2ban'],
+    package { 'net-analyzer/fail2ban':
+      ensure => installed,
     }
+    ->
+    file {
+      default:
+        mode  => '0644',
+        owner => 'root',
+        group => 'root',
+      ;
 
+      '/etc/fail2ban/jail.d':
+        ensure => directory,
+      ;
+
+      '/etc/fail2ban/jail.d/sshd.conf':
+        content => $sshd_jail_conf,
+      ;
+    }
+    ~>
     service { 'fail2ban':
       enable => true,
     }
   } else {
     service { 'fail2ban':
+      ensure => stopped,
       enable => false,
     }
-
-    file { '/etc/fail2ban/jail.d/sshd.conf':
-      ensure  => absent,
+    ->
+    file { '/etc/fail2ban/jail.d':
+      ensure => absent,
+      force  => true,
     }
-
+    ->
     package { 'net-analyzer/fail2ban':
       ensure  => absent,
-      require => [
-        Service['fail2ban'],
-        File['/etc/fail2ban/jail.d/sshd.conf'],
-      ],
     }
   }
 }
