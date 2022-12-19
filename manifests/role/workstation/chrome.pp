6b86d40de8125bfd17f2368cbe61c0c0d75acf27
James Lee <james@james.tl>
2022-12-19 12:05:27 -0500

    Revert "chrome: Workaround transparent borders with picom"
    Transparent border problem fixed in xmonad 0.17.1.
    
    This reverts commit 2b3625808e3ab529fd6da306438189316a28891b.

--- manifests/role/workstation/chrome.pp
+++ manifests/role/workstation/chrome.pp
@@ -44,9 +44,6 @@ class nest::role::workstation::chrome (
             export GOOGLE_API_KEY='${google_api_key}'
             export GOOGLE_DEFAULT_CLIENT_ID='${google_oauth_id}'
             export GOOGLE_DEFAULT_CLIENT_SECRET='${google_oauth_secret}'
-
-            # Force picom to restart to workaround transparent window borders
-            pgrep -u \$USER -x chrome || pkill picom
             | EOT
 
           file { '/etc/chromium/nest':
@@ -62,12 +59,8 @@ class nest::role::workstation::chrome (
           ensure => absent,
         }
       } else {
-        $chrome_wrapper = @("WRAPPER"/$)
+        $chrome_wrapper = @("WRAPPER")
           #!/bin/bash
-
-          # Force picom to restart to workaround transparent window borders
-          pgrep -u \$USER -x chrome || pkill picom
-
           exec /opt/google/chrome/google-chrome --${gpu_rasterization_flag} --force-dark-mode --enable-features=WebUIDarkMode "$@"
           | WRAPPER
 
