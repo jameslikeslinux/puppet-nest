e2ff9d462873376d1ed657faeafc2e1a8450d6ac
James Lee <james@james.tl>
2022-12-19 22:07:06 -0500

    media: Detect presence of 'vaapi' USE flag

--- manifests/role/workstation/media.pp
+++ manifests/role/workstation/media.pp
@@ -6,9 +6,13 @@ class nest::role::workstation::media {
     ensure => installed,
   }
 
-  if $facts['profile']['platform'] == 'haswell' {
-    package { 'media-video/libva-utils':
-      ensure => installed,
-    }
+  if 'vaapi' in $facts['portage_use'].split(' ') {
+    $libva_utils_ensure = installed
+  } else {
+    $libva_utils_ensure = absent
+  }
+
+  package { 'media-video/libva-utils':
+    ensure => $libva_utils_ensure,
   }
 }
