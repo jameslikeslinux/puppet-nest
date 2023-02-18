3c41f21580a37e8417c2638ff1c78ae9dfa526ed
James Lee <james@james.tl>
2023-02-18 03:27:43 -0500

    Revert "dnsmasq: Remove systemd drop-in in favor of 'bind-dynamic'"
    Doesn't work with Podman containers (probably configured to talk to
    172.22.0.1). Not worth figuring out.
    
    This reverts commit 100397a3eb595b1a6e3ad2cce2aa8b5b8c21aefd.

--- manifests/service/dnsmasq.pp
+++ manifests/service/dnsmasq.pp
@@ -26,7 +26,14 @@ class nest::service::dnsmasq (
     enable => true,
   }
 
+  # Ensure dnsmasq can bind to its requested interfaces
   if $interfaces.empty {
+    file { '/etc/systemd/system/dnsmasq.service.d':
+      ensure => absent,
+      force  => true,
+      notify => Nest::Lib::Systemd_reload['dnsmasq'],
+    }
+
     file { '/etc/dnsmasq.d/interfaces.conf':
       ensure => absent,
       notify => Service['dnsmasq'],
@@ -35,12 +42,34 @@ class nest::service::dnsmasq (
     $dnsmasq_interfaces = $interfaces.map |$i| { "interface=${i}" }.join("\n")
     $dnsmasq_interfaces_conf = @("END_CONF")
       ${dnsmasq_interfaces}
-      bind-dynamic
+      bind-interfaces
       | END_CONF
 
     file { '/etc/dnsmasq.d/interfaces.conf':
       content => $dnsmasq_interfaces_conf,
       notify  => Service['dnsmasq'],
     }
+
+    $systemd_device_units = $interfaces.map |$i| { "sys-subsystem-net-devices-${i}.device" }.join(' ')
+    $dnsmasq_systemd_dropin_unit = @("END_UNIT")
+      [Unit]
+      Requires=${systemd_device_units}
+      After=${systemd_device_units}
+      | END_UNIT
+
+    file { '/etc/systemd/system/dnsmasq.service.d':
+      ensure => directory,
+      mode   => '0755',
+    }
+
+    file { '/etc/systemd/system/dnsmasq.service.d/10-interfaces.conf':
+      mode    => '0644',
+      content => $dnsmasq_systemd_dropin_unit,
+      notify  => Nest::Lib::Systemd_reload['dnsmasq'],
+    }
+  }
+
+  nest::lib::systemd_reload { 'dnsmasq':
+    notify => Service['dnsmasq'],
   }
 }
