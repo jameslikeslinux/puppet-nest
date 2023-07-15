dc30a077184033e4e54fced9135a506d8f512d74
James Lee <james@james.tl>
2023-07-14 22:22:14 -0400

    kernel: Fix llvm_ld fact to source /etc/profile

--- lib/facter/llvm_ld.rb
+++ lib/facter/llvm_ld.rb
@@ -1,6 +1,6 @@
 Facter.add('llvm_ld') do
   confine kernel: 'Linux'
   setcode do
-    Facter::Core::Execution.execute('which ld.lld')
+    Facter::Core::Execution.execute('bash -c "source /etc/profile && which ld.lld"')
   end
 end
