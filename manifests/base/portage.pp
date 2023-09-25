16fc877b8792ee182e86dbcad717248dd92395f0
James Lee <james@james.tl>
2023-09-25 12:47:32 -0400

    portage: Build chromium with less parellelism
    Avoid resource exhaustion building this huge package.

--- manifests/base/portage.pp
+++ manifests/base/portage.pp
@@ -150,11 +150,25 @@ class nest::base::portage {
   # Package environments and properties
   #
 
-  file { '/etc/portage/env/no-buildpkg.conf':
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
-    content => "FEATURES=\"-buildpkg\"\n",
+  $makejobs_huge = $makejobs / 2
+  file {
+    default:
+      mode  => '0644',
+      owner => 'root',
+      group => 'root',
+    ;
+
+    '/etc/portage/env/no-buildpkg.conf':
+      content => "FEATURES=\"-buildpkg\"\n",
+    ;
+
+    '/etc/portage/env/huge.conf':
+      content => "MAKEOPTS=\"-j${makejobs_huge} -l${loadlimit}\"\n",
+    ;
+  }
+  ->
+  package_env { 'www-client/chromium':
+    env => 'huge.conf',
   }
 
   # xvid incorrectly passes `-mcpu` as `-mtune` which doesn't accept `+crypto`
