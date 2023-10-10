7f627a66dd4a0a194ee3953c3ead3f303ce56fb5
James Lee <james@james.tl>
2023-10-10 13:49:15 -0400

    bluetooth: Confine to Gentoo-based hosts

--- manifests/service/bluetooth.pp
+++ manifests/service/bluetooth.pp
@@ -1,24 +1,26 @@
 class nest::service::bluetooth {
-  nest::lib::package { 'net-wireless/bluez':
-    ensure => installed,
-  }
-  ->
-  service { 'bluetooth':
-    enable => true,
-  }
-
-  if $facts['profile']['platform'] == 'raspberrypi4' {
-    file { '/etc/systemd/system/btattach.service':
-      mode   => '0644',
-      owner  => 'root',
-      group  => 'root',
-      source => 'puppet:///modules/nest/bluetooth/btattach.service',
+  if $facts['os']['family'] == 'Gentoo' {
+    nest::lib::package { 'net-wireless/bluez':
+      ensure => installed,
     }
-    ~>
-    ::nest::lib::systemd_reload { 'bluetooth': }
     ->
-    service { 'btattach':
-      enable  => true,
+    service { 'bluetooth':
+      enable => true,
+    }
+
+    if $facts['profile']['platform'] == 'raspberrypi4' {
+      file { '/etc/systemd/system/btattach.service':
+        mode   => '0644',
+        owner  => 'root',
+        group  => 'root',
+        source => 'puppet:///modules/nest/bluetooth/btattach.service',
+      }
+      ~>
+      ::nest::lib::systemd_reload { 'bluetooth': }
+      ->
+      service { 'btattach':
+        enable  => true,
+      }
     }
   }
 }
