eda98dc694afa2784fe2a7c73cb04abf1347b81f
James Lee <james@james.tl>
2023-10-22 01:01:11 -0400

    puppet: Update Puppet Server to 8.2.3

--- manifests/service/puppet.pp
+++ manifests/service/puppet.pp
@@ -65,7 +65,7 @@ class nest::service::puppet (
   ->
   nest::lib::container { 'puppetserver':
     pod     => 'puppet',
-    image   => 'ghcr.io/voxpupuli/container-puppetserver:7.13.0',
+    image   => 'registry.gitlab.james.tl/nest/forks/container-puppetserver:8.2.3',
     env     => [
       'PUPPETSERVER_HOSTNAME=puppet',
       'DNS_ALT_NAMES=puppet.nest',
