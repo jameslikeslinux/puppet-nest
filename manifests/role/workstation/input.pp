467f6ae79b1f94407c8d127d48774a0f12911c81
James Lee <james@james.tl>
2023-12-28 13:05:09 -0500

    input: Add override quirks for MX Master 3S scrolling

--- manifests/role/workstation/input.pp
+++ manifests/role/workstation/input.pp
@@ -23,10 +23,9 @@ class nest::role::workstation::input {
 
   file {
     default:
-      mode   => '0644',
-      owner  => 'root',
-      group  => 'root',
-      notify => Exec['udev-hwdb-update'],
+      mode  => '0644',
+      owner => 'root',
+      group => 'root',
     ;
 
     '/etc/udev/hwdb.d/61-evdev-local.hwdb':
@@ -39,13 +38,33 @@ class nest::role::workstation::input {
       content => $keyboard_hwdb,
     ;
   }
-
+  ~>
   exec { 'udev-hwdb-update':
     command     => '/bin/udevadm hwdb --update',
     refreshonly => true,
   }
 
-  file { '/etc/X11/xinit/xinitrc.d/99-xmodmap':
-    ensure => absent,
+  # Fix horizontal scrolling on MX Master 3S
+  $libinput_quirks = @(QUIRKS)
+    [Logitech Bolt Receiver]
+    MatchVendor=0x046D
+    MatchProduct=0xC548
+    ModelInvertHorizontalScrolling=1
+    | QUIRKS
+
+  file {
+    default:
+      mode  => '0644',
+      owner => 'root',
+      group => 'root',
+    ;
+
+    '/etc/libinput':
+      ensure => directory,
+    ;
+
+    '/etc/libinput/local-overrides.quirks':
+      content => $libinput_quirks,
+    ;
   }
 }
