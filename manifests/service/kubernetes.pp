caa8f9ddcf820634f87ff87efe9581af8d19450b
James Lee <james@james.tl>
2024-01-17 03:04:12 -0500

    k8s: Calico inits its own CNI config now

--- manifests/service/kubernetes.pp
+++ manifests/service/kubernetes.pp
@@ -22,14 +22,6 @@ class nest::service::kubernetes (
     enable => true,
   }
 
-  # Provide initial CNI config so CoreDNS doesn't deploy to Podman network.
-  # CRI-O picks this up dynamically if it's running.
-  file { '/etc/cni/net.d/10-calico.conflist':
-    replace => false,
-    source  => 'puppet:///modules/nest/kubernetes/cni-conf.json',
-    require => Package['app-containers/cri-o'],
-  }
-
   # Install and enable kubelet with a service that works with CRI-O and kubeadm
   package { 'sys-cluster/kubelet':
     ensure => installed,
