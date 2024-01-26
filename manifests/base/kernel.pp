0c5613c97192a4e7b425209512a823f797ada8e1
James Lee <james@james.tl>
2024-01-26 13:12:44 -0500

    Revert "kernel: Fix radxa kernel build with OpenSSL 3.0"
    Warning can't be controlled with environment variables. Fixed in the
    kernel's `scripts/Makefile`.
    
    This reverts commit 74be310ef7358f46e9ede82bb907ee3e750bbdc0.

--- manifests/base/kernel.pp
+++ manifests/base/kernel.pp
@@ -81,7 +81,6 @@ class nest::base::kernel {
 
     # Ignore warning on newer GCC
     $cflags = [
-      '-Wno-deprecated-declarations',
       '-Wno-implicit-fallthrough',
       '-Wno-tautological-compare',
       '-Wno-int-in-bool-context',
