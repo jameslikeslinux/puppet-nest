e68b05ac66ed73c573717087615fd459cda795ee
James Lee <james@james.tl>
2024-01-28 19:23:17 -0500

    builders: Enable qemu-user
    Amlogic FIP contains x86_64 binaries I need to run on aarch64.

--- manifests/tool/qemu.pp
+++ manifests/tool/qemu.pp
@@ -7,25 +7,17 @@ class nest::tool::qemu {
         'static-user',
         'qemu_user_targets_arm',
         'qemu_user_targets_aarch64',
+        'qemu_user_targets_x86_64',
       ],
     }
-
-    # lint:ignore:140chars
-    $binfmt_conf = @(EOT)
-      :qemu-arm:M::\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x28\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-arm:OC
-      :qemu-aarch64:M::\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xb7\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-aarch64:OC
-      | EOT
-    # lint:endignore
-
+    ->
     file { '/etc/binfmt.d/qemu.conf':
       mode    => '0644',
       owner   => 'root',
       group   => 'root',
-      content => $binfmt_conf,
-      notify  => Service['systemd-binfmt'],
-      require => Package['app-emulation/qemu'],
+      source  => '/usr/share/qemu/binfmt.d/qemu.conf',
     }
-
+    ~>
     service { 'systemd-binfmt':
       ensure => running,
     }
