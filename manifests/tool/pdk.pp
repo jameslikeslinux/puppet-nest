d4160e99039ad10004cd9a3009e077fdb2161e10
James Lee <james@james.tl>
2024-08-13 13:47:18 -0400

    pdk: Bump to 3.2.0

--- manifests/tool/pdk.pp
+++ manifests/tool/pdk.pp
@@ -1,6 +1,6 @@
 class nest::tool::pdk {
   if $facts['build'] == 'pdk' {
-    $pdk_version        = '3.0.1'
+    $pdk_version        = '3.2.0'
     $ruby_minor_version = $facts['ruby']['version'].regsubst('^(\d+\.\d+).*', '\1')
 
     package { 'pdk':
