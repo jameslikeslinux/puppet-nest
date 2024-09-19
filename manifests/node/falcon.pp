6d432f23a7792d7afc079168679f4242490808e5
James Lee <james@james.tl>
2024-09-19 00:10:19 -0400

    falcon: Install vendor-reset for AMD GPU passthrough
    Update pci_stub IDs.

--- manifests/node/falcon.pp
+++ manifests/node/falcon.pp
@@ -12,4 +12,10 @@ class nest::node::falcon {
       gcc_only => true,
     ;
   }
+
+  nest::lib::package { 'app-emulation/vendor-reset':
+    ensure  => installed,
+    require => Class['nest::base::kernel'], # for CONFIG_FUNCTION_TRACER
+    notify  => Class['nest::base::dracut'], # package installs module-load config
+  }
 }
