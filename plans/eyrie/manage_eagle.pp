1f9db972ccf2cafff6c1a2ed5c03ebe43dfc3bcc
James Lee <james@james.tl>
2024-11-24 03:09:11 -0500

    eagle: Update plan to manage chassis
    Interact with the PowerSupplyControl sketch through gpio commands.

--- plans/eyrie/manage_eagle.pp
+++ plans/eyrie/manage_eagle.pp
@@ -1,12 +1,14 @@
 # Manage the state of Eagle nodes through its console
 #
-# @param targets Nodes to manage
+# @param targets Nodes to manage or none to manage the chassis
 # @param power Action to take on the nodes
 plan nest::eyrie::manage_eagle (
-  TargetSpec $targets,
-  Enum['off', 'on', 'reset'] $power,
+  Enum['off', 'on', 'reset', 'status'] $power,
+  Optional[TargetSpec] $targets = undef,
 ) {
-  $name_to_pin = {
+  $pson_pin      = 17
+  $pscontrol_pin = 27
+  $name_to_pin   = {
     'eagle'   => 18,
     'eaglet1' => 23,
     'eaglet2' => 24,
@@ -16,31 +18,83 @@ plan nest::eyrie::manage_eagle (
     'eaglet6' => 12,
   }
 
-  get_targets($targets).each |$node| {
-    $pin = $name_to_pin[$node.name]
-    if $pin {
-      case $power {
-        'off': {
-          $gpio_cmd = "gpioset -l 0 ${pin}=0"
-          $action   = 'Power off'
-        }
+  if $targets {
+    get_targets($targets).each |$node| {
+      $pin = $name_to_pin[$node.name]
+      if $pin {
+        case $power {
+          'off': {
+            run_command("gpioset -l 0 ${pin}=0", 'eagle-console', "Power off node ${node.name}", {
+              _run_as => 'root',
+            })
+          }
 
-        'on': {
-          $gpio_cmd = "gpioset -l 0 ${pin}=1"
-          $action   = 'Power on'
-        }
+          'on': {
+            run_command("gpioset -l 0 ${pin}=1", 'eagle-console', "Power on node ${node.name}", {
+              _run_as => 'root',
+            })
+          }
 
-        'reset': {
-          $gpio_cmd = "gpioset -l -m time -s 1 0 ${pin}=0 && gpioset -l 0 ${pin}=1"
-          $action   = 'Reset'
+          'reset': {
+            run_command("gpioset -l -m time -s 1 0 ${pin}=0 && gpioset -l 0 ${pin}=1", 'eagle-console', "Reset node ${node.name}", {
+              _run_as => 'root',
+            })
+          }
+
+          default: {
+            fail("Can't use power=${power} on ${node.name}")
+          }
+        }
+      } else {
+        out::message("Cannot manage node ${node.name} from this console")
+      }
+    }
+  } else {
+    $pson = run_command("gpioget 0 ${pson_pin}", 'eagle-console', "Get current power status", {
+      _run_as => 'root',
+    }).first.value['stdout'].str2bool
+
+    case $power {
+      'off': {
+        if $pson {
+          run_command("gpioset -m time -u 200000 0 ${pscontrol_pin}=1 && gpioset 0 ${pscontrol_pin}=0", 'eagle-console', 'Power off the chassis', {
+            _run_as => 'root',
+          })
+        } else {
+          out::message('The chassis is already off')
         }
       }
 
-      run_command($gpio_cmd, 'eagle-console', "${action} node ${node.name}", {
-        _run_as => 'root',
-      })
-    } else {
-      log::error("Cannot manage node ${node.name} from this console")
+      'on': {
+        if !$pson {
+          run_command("gpioset -m time -u 200000 0 ${pscontrol_pin}=1 && gpioset 0 ${pscontrol_pin}=0", 'eagle-console', 'Power on the chassis', {
+            _run_as => 'root',
+          })
+        } else {
+          out::message('The chassis is already on')
+        }
+      }
+
+      'reset': {
+        if $pson {
+          run_command("gpioset -m time -u 200000 0 ${pscontrol_pin}=1 && gpioset -m time -s 1 0 ${pscontrol_pin}=0 && gpioset -m time -u 200000 0 ${pscontrol_pin}=1 && gpioset 0 ${pscontrol_pin}=0", 'eagle-console', 'Reset off the chassis', {
+            _run_as => 'root',
+          })
+        } else {
+          out::message('The chassis is off')
+        }
+      }
+
+      'status': {
+        if $pson {
+          $power_state = 'on'
+        } else {
+          $power_state = 'off'
+        }
+
+        out::message("The chassis is ${power_state}")
+        return $power_state
+      }
     }
   }
 }
