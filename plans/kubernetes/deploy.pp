5a40710239e8e6cec2d29d36632524282172a187
James Lee <james@james.tl>
2024-12-05 04:35:35 -0500

    k8s: Ceph cluster improvements checkpoint

--- plans/kubernetes/deploy.pp
+++ plans/kubernetes/deploy.pp
@@ -29,125 +29,128 @@ plan nest::kubernetes::deploy (
   Boolean          $wait      = false,
   Array[Hash]      $subcharts = [],
   Optional[String] $parent    = undef,
+  Boolean          $deploy    = true,
 ) {
-  $subcharts.each |$subchart| {
-    run_plan('nest::kubernetes::deploy', $subchart + {
-      parent    => $service,
-      namespace => $namespace,
-      render_to => '/tmp/kustomize/subcharts.yaml',
-      append    => true,
-    })
-  }
+  if $deploy {
+    $subcharts.each |$subchart| {
+      run_plan('nest::kubernetes::deploy', $subchart + {
+        parent    => $service,
+        namespace => $namespace,
+        render_to => '/tmp/kustomize/subcharts.yaml',
+        append    => true,
+      })
+    }
 
-  if $repo_name and $repo_url {
-    $chart_real = "${repo_name}/${chart}"
-    $helm_repo_add_cmd = "helm repo add ${repo_name} ${repo_url}"
-    run_command($helm_repo_add_cmd, 'localhost', "Add Helm repo ${repo_name} at ${repo_url}")
-  } else {
-    $chart_real = $chart
-  }
-
-  # Because YAML plans
-  if $render_to and $render_to != '' {
-    $render_to_real = $render_to
-  } else {
-    $render_to_real = undef
-  }
-
-  apply('localhost') {
-    $kubernetes_service        = $service
-    $kubernetes_app            = $app
-    $kubernetes_namespace      = $namespace
-    $kubernetes_parent_service = $parent
-
-    include nest::bolt # for lookups
-
-    if $restore {
-      $backup_resources = ['backup']
+    if $repo_name and $repo_url {
+      $chart_real = "${repo_name}/${chart}"
+      $helm_repo_add_cmd = "helm repo add ${repo_name} ${repo_url}"
+      run_command($helm_repo_add_cmd, 'localhost', "Add Helm repo ${repo_name} at ${repo_url}")
     } else {
-      $backup_resources = []
+      $chart_real = $chart
     }
 
-    $resources = lookup('resources') - $backup_resources
-    $values    = lookup('values')
-    $patches   = lookup('patches')
-
-    file {
-      '/tmp/kustomize':
-        ensure => directory;
-      '/tmp/kustomize/subcharts.yaml':
-        ensure => file;
-      '/tmp/kustomize/resources.yaml':
-        content => $resources.values.flatten.map |$r| { if $r.empty { '' } else { $r.stdlib::to_yaml } }.join;
-      '/tmp/kustomize/values.yaml':
-        content => $values.stdlib::to_yaml;
-      '/tmp/kustomize/kustomization.yaml':
-        content => {
-          'resources' => ['subcharts.yaml', 'resources.yaml', 'helm.yaml'],
-          'patches'   => $patches.keys.sort.map |$k| { $patches[$k] }.flatten.map |$p| {
-            if $p['patch'] =~ String {
-              $p
-            } else {
-              $p + { 'patch' => $p['patch'].stdlib::to_yaml }
-            }
-          },
-        }.stdlib::to_yaml,
-      ;
-    }
-  }
-
-  $helm_cmd = [
-    'helm',
-
-    $render_to_real ? {
-      undef   => ['upgrade', '--install'],
-      default => ['template', '--kube-version', '1.28.2'],
-    },
-
-    $service, $chart_real,
-
-    $hooks ? {
-      false   => '--no-hooks',
-      default => [],
-    },
-
-    $namespace ? {
-      undef   => [],
-      default => ['--create-namespace', '--namespace', $namespace],
-    },
-
-    '--post-renderer', './scripts/kustomize.sh',
-    '--post-renderer-args', '/tmp/kustomize',
-    '--values', '/tmp/kustomize/values.yaml',
-
-    $version ? {
-      undef   => [],
-      default => ['--version', $version],
-    },
-
-    ($wait and !$render_to_real) ? {
-      true    => ['--wait', '--timeout', '1h'],
-      default => [],
-    },
-  ].flatten.shellquote
-
-  if $render_to_real {
-    if $append {
-      $redirect_op = '>>'
+    # Because YAML plans
+    if $render_to and $render_to != '' {
+      $render_to_real = $render_to
     } else {
-      $redirect_op = '>'
+      $render_to_real = undef
     }
-    $redirect = " ${redirect_op} ${render_to_real.shellquote}"
-    $cmd_verb = 'Render'
-  } else {
-    $redirect = ''
-    $cmd_verb = 'Deploy'
-  }
 
-  run_command("${helm_cmd}${redirect}", 'localhost', "${cmd_verb} ${service} from Helm chart ${chart_real}")
+    apply('localhost') {
+      $kubernetes_service        = $service
+      $kubernetes_app            = $app
+      $kubernetes_namespace      = $namespace
+      $kubernetes_parent_service = $parent
 
-  # Give time for VIP to propagate
-  if $wait {
-    ctrl::sleep(10)
+      include nest::bolt # for lookups
+
+      if $restore {
+        $backup_resources = ['backup']
+      } else {
+        $backup_resources = []
+      }
+
+      $resources = lookup('resources') - $backup_resources
+      $values    = lookup('values')
+      $patches   = lookup('patches')
+
+      file {
+        '/tmp/kustomize':
+          ensure => directory;
+        '/tmp/kustomize/subcharts.yaml':
+          ensure => file;
+        '/tmp/kustomize/resources.yaml':
+          content => $resources.values.flatten.map |$r| { if $r.empty { '' } else { $r.stdlib::to_yaml } }.join;
+        '/tmp/kustomize/values.yaml':
+          content => $values.stdlib::to_yaml;
+        '/tmp/kustomize/kustomization.yaml':
+          content => {
+            'resources' => ['subcharts.yaml', 'resources.yaml', 'helm.yaml'],
+            'patches'   => $patches.keys.sort.map |$k| { $patches[$k] }.flatten.map |$p| {
+              if $p['patch'] =~ String {
+                $p
+              } else {
+                $p + { 'patch' => $p['patch'].stdlib::to_yaml }
+              }
+            },
+          }.stdlib::to_yaml,
+        ;
+      }
+    }
+
+    $helm_cmd = [
+      'helm',
+
+      $render_to_real ? {
+        undef   => ['upgrade', '--install'],
+        default => ['template', '--kube-version', '1.28.2'],
+      },
+
+      $service, $chart_real,
+
+      $hooks ? {
+        false   => '--no-hooks',
+        default => [],
+      },
+
+      $namespace ? {
+        undef   => [],
+        default => ['--create-namespace', '--namespace', $namespace],
+      },
+
+      '--post-renderer', './scripts/kustomize.sh',
+      '--post-renderer-args', '/tmp/kustomize',
+      '--values', '/tmp/kustomize/values.yaml',
+
+      $version ? {
+        undef   => [],
+        default => ['--version', $version],
+      },
+
+      ($wait and !$render_to_real) ? {
+        true    => ['--wait', '--timeout', '1h'],
+        default => [],
+      },
+    ].flatten.shellquote
+
+    if $render_to_real {
+      if $append {
+        $redirect_op = '>>'
+      } else {
+        $redirect_op = '>'
+      }
+      $redirect = " ${redirect_op} ${render_to_real.shellquote}"
+      $cmd_verb = 'Render'
+    } else {
+      $redirect = ''
+      $cmd_verb = 'Deploy'
+    }
+
+    run_command("${helm_cmd}${redirect}", 'localhost', "${cmd_verb} ${service} from Helm chart ${chart_real}")
+
+    # Give time for VIP to propagate
+    if $wait {
+      ctrl::sleep(10)
+    }
   }
 }
