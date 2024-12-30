670aa775df15ecc3b7cf83d05c9ce8bbcbb850b6
James Lee <james@james.tl>
2024-12-30 04:10:13 -0500

    packages: Install vscode on compatible workstations

--- manifests/gui/packages.pp
+++ manifests/gui/packages.pp
@@ -7,4 +7,10 @@ class nest::gui::packages {
   nest::lib::package { 'net-dialup/minicom':
     ensure => installed,
   }
+
+  if $facts['profile']['architecture'] in ['amd64', 'arm', 'arm64'] {
+    nest::lib::package { 'app-editors/vscode':
+      ensure => installed,
+    }
+  }
 }
